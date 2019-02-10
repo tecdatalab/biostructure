d800208ec6642d4d2fa9da8371bbc1cfd9e000b1
JuViquez <jviquez2@gmail.com>
2019-02-09 18:32:40 -0600

    components diectory created

--- emsurfer/src/app/app.module.ts
+++ emsurfer/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HeaderComponent} from './header/header.component'
+import { HeaderComponent} from './components/header/header.component'
 
 import { AppComponent } from './app.component';
-import { ContourShapeInputComponent } from './contour-shape-input/contour-shape-input.component';
+import { ContourShapeInputComponent } from './components/contour-shape-input/contour-shape-input.component';
+import { UploadEmMapComponent } from './components/upload-em-map/upload-em-map.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContourShapeInputComponent
+    ContourShapeInputComponent,
+    UploadEmMapComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule
---
+++ emsurfer/src/app/components/upload-em-map/upload-em-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-em-map',
+  templateUrl: './upload-em-map.component.html',
+  styleUrls: ['./upload-em-map.component.css']
+})
+export class UploadEmMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
