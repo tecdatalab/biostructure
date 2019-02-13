ec441a22822b14b65df25696f50648951da803ff
JuViquez <jviquez2@gmail.com>
2019-02-13 10:23:25 -0600

    app header added

--- emsurfer/src/app/app.module.ts
+++ emsurfer/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HeaderComponent} from './header/header.component'
-
 import { AppComponent } from './app.component';
+import { HeaderComponent} from './components/header/header.component'
 
 @NgModule({
   declarations: [
