ee2f663288f9e94e364463660df57d9a357ebe28
joseant12 <joseantonio122@gmail.com>
2019-02-13 00:37:30 -0600

    Testing search-form component

--- emsurfer/src/app/components/contour-shape-input/contour-shape-input.component.spec.ts
+++ emsurfer/src/app/components/contour-shape-input/contour-shape-input.component.spec.ts
@@ -18,8 +18,8 @@ describe('ContourShapeInputComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  /*
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
+  });*/
 });
