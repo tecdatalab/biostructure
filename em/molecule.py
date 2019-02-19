3b7eac9eb25496c56f0c2369e819f3f0aa607c83
manu3193 <manu3193@gmail.com>
2019-02-19 01:12:08 -0600

    code refactor

---
+++ em/molecule.py
@@ -0,0 +1,18 @@
+
+
+
+
+class Molecule():
+
+
+    def __init__(self, rawHeader, data, size, start, grid_size, cell_dim, density_ranges, origin):
+        self.rawHeader = rawHeader
+        self.data = data
+        self.size = size
+        self.start = start
+        self.grid_size = grid_size
+        self.cell_dim = cell_dim
+        self.density_ranges = density_ranges
+        self.origin = origin
+    
+    
