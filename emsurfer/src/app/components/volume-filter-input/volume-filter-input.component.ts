3a82add49e9552155d9034725656626e0aed8c45
JuViquez <jviquez2@gmail.com>
2019-02-19 20:31:11 -0600

    removed unused imports

--- emsurfer/src/app/components/volume-filter-input/volume-filter-input.component.ts
+++ emsurfer/src/app/components/volume-filter-input/volume-filter-input.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-volume-filter-input',
   templateUrl: './volume-filter-input.component.html',
   styleUrls: ['./volume-filter-input.component.css']
 })
-export class VolumeFilterInputComponent implements OnInit {
+export class VolumeFilterInputComponent {
 
   @Input() parentForm: FormGroup;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
