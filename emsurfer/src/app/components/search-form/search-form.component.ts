a9b23645d59e537434759ee7954e5bedce19e0cc
JuViquez <jviquez2@gmail.com>
2019-02-19 20:29:07 -0600

    fixed search-form component cb error

--- emsurfer/src/app/components/search-form/search-form.component.ts
+++ emsurfer/src/app/components/search-form/search-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -8,27 +8,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-
   searchForm: FormGroup;
   defaultFormState: any;
   cbEmdb: boolean;
 
-  constructor(private fb: FormBuilder, private router: Router) { 
+  constructor(private fb: FormBuilder, private router: Router) {
     this.cbEmdb = true;
   }
 
   ngOnInit() {
     const queryGroup = this.fb.group({
-      emdb_id: ['1884',[
-        Validators.required,
-        Validators.minLength(4),
-        Validators.pattern('^[0-9]*$')
-      ]],
+      emdb_id: [
+        '1884',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.pattern('^[0-9]*$')
+        ]
+      ],
       em_map: this.fb.group({
         file: null,
         contour_level: '3.14'
-        })
-      });
+      })
+    });
 
     const rfGroup = this.fb.group({
       min: null,
@@ -43,47 +45,53 @@ export class SearchFormComponent implements OnInit {
     });
 
     this.defaultFormState = this.searchForm.getRawValue();
-    console.log(this.searchForm.get('query').get('emdb_id').value);
-    this.searchForm.valueChanges.subscribe(console.log);
   }
 
   submitHandler() {
-    const params = 'query/' +
-                this.searchForm.get('query').get('emdb_id').value +
-                '/' + this.searchForm.get('volume_filter').value +
-                '/' + this.searchForm.get('resolution_filter').get('min').value +
-                '/' + this.searchForm.get('resolution_filter').get('max').value;
+    const params =
+      'query/' +
+      this.searchForm.get('query').get('emdb_id').value +
+      '/' +
+      this.searchForm.get('volume_filter').value +
+      '/' +
+      this.searchForm.get('resolution_filter').get('min').value +
+      '/' +
+      this.searchForm.get('resolution_filter').get('max').value;
     this.router.navigateByUrl(params);
   }
 
   cbEmdbChange() {
     this.cbEmdb = !this.cbEmdb;
-    console.log(this.cbEmdb);
-    console.log(this.searchForm.get('query').get('emdb_id').value);
     if (this.cbEmdb) {
-      this.searchForm.get('query').get('emdb_id').setValidators([
-        Validators.required,
-        Validators.minLength(4),
-        Validators.pattern('^[0-9]*$')
-      ])
+      this.searchForm
+        .get('query')
+        .get('emdb_id')
+        .setValidators([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.pattern('^[0-9]*$')
+        ]);
       this.searchForm.patchValue({
         query: {
           em_map: {
-            file : null
+            file: null
           }
         }
       });
     } else {
-      this.searchForm.get('query').get('emdb_id').setValidators(null);
-      this.searchForm.get('query').get('emdb_id').updateValueAndValidity();
+      this.searchForm
+        .get('query')
+        .get('emdb_id')
+        .setValidators(null);
     }
+    this.searchForm
+      .get('query')
+      .get('emdb_id')
+      .updateValueAndValidity();
   }
 
   reset() {
     this.searchForm.patchValue(this.defaultFormState);
     this.cbEmdb = true;
   }
-
 }
-
-
