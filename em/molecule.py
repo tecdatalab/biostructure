46d59df09acde3d821d56b2d4deea5fb227f0935
Manuel Zumbado <manu3193@gmail.com>
2019-02-21 11:50:48 -0600

    add molecule methods

--- em/molecule.py
+++ em/molecule.py
@@ -5,13 +5,35 @@ class Molecule():
     def __init__(self, rawHeader, data, size, start, grid_size, cell_dim, density_ranges, origin):
         self.rawHeader = rawHeader
         self.data = data
-        self.size = size
-        self.start = start
-        self.grid_size = grid_size
-        self.cell_dim = cell_dim
-        self.density_ranges = density_ranges
-        self.origin = origin
+        (self.ny, self.nx, self.nz) = size
+        (self.nystart, self.nxstart, self.nzstart) = start
+        (self.my, self.mx, self.mz) = grid_size
+        (self.ylen, self.xlen, self.zlen) = cell_dim
+        (self.dmin, self.dmax, self.dmean) = density_ranges
+        (self.yorigin, self.xorigin, self.zorigin) = origin
 
     
-    def getDataArray(self):
+    def data(self):
         return self.data
+
+
+    def shape(self):
+        return (self.ny, self.nx, self.nz)
+
+
+    def start_point(self):
+        return (self.nystart, self.nxstart, self.nzstart)
+
+    def grid_size(self):
+        return (self.my, self.mx, self.mz)
+
+    def cell_dim(self):
+        return (self.ylen, self.xlen, self.zlen)
+
+    def density_range(self):
+        return (self.dmin, self.dmax, self.dmean)
+
+    def origin(self):
+        return (self.yorigin, self.xorigin, self.zorigin)
+
+
