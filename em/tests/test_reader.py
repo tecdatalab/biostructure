6b0c20556c586bc80a7167485876abdbf4696682
Manuel Zumbado <manu3193@gmail.com>
2019-02-21 15:07:24 -0600

    add read test

--- em/tests/test_reader.py
+++ em/tests/test_reader.py
@@ -7,3 +7,15 @@ class TestReader(unittest.TestCase):
     def test_init(self):
         with self.assertRaises(IOError):
             myreader = rd.Reader('somefile.map')
+
+
+    def test_read(self):
+        myreader = rd.Reader('tests/EMD-sample.map')
+        mymolecule = myreader.read()
+        
+        self.assertEqual(mymolecule.shape,(140,140,140))
+        self.assertEqual(mymolecule.start_point, (0,0,0))
+        self.assertEqual(mymolecule.grid_size, (140,140,140))
+        self.assertEqual(mymolecule.cell_dim, (246.4, 246.4, 246.4))
+        self.assertEqual(mymolecule.density_range, (-0.24971596896648407,0.4126806855201721,5.458949090098031e-05))
+        self.assertEqual(mymolecule.origin, (0., 0., 0.))
