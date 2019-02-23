27e78accd301dd1b9ddc3fc185947562e94c4425
manu3193 <manu3193@gmail.com>
2019-02-23 16:07:31 -0600

    add test for __eq__ method

--- em/tests/test_reader.py
+++ em/tests/test_reader.py
@@ -11,11 +11,9 @@ class TestReader(unittest.TestCase):
 
     def test_read(self):
         myreader = rd.Reader('tests/EMD-sample.map')
+        otherreader = rd.Reader('tests/EMD-sample.map')
         mymolecule = myreader.read()
+        othermolecule = otherreader.read()
         
-        self.assertEqual(mymolecule.shape,(140,140,140))
-        self.assertEqual(mymolecule.start_point, (0,0,0))
-        self.assertEqual(mymolecule.grid_size, (140,140,140))
-        self.assertEqual(mymolecule.cell_dim, (246.4, 246.4, 246.4))
-        self.assertEqual(mymolecule.density_range, (-0.24971596896648407,0.4126806855201721,5.458949090098031e-05))
-        self.assertEqual(mymolecule.origin, (0., 0., 0.))
+        self.assertEqual(mymolecule, othermolecule)
+ 
