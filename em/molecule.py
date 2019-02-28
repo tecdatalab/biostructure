db3f70b7d271ed7078131464c4401fd92c7977be
Manuel Zumbado <manu3193@gmail.com>
2019-02-28 10:20:30 -0600

    fix axis order in functions

--- em/molecule.py
+++ em/molecule.py
@@ -10,7 +10,7 @@ class Molecule():
         (self.my, self.mx, self.mz) = grid_size
         (self.ylen, self.xlen, self.zlen) = cell_dim
         (self.dmin, self.dmax, self.dmean) = density_ranges
-        (self.yorigin, self.xorigin, self.zorigin) = origin
+        (self.zorigin, self.yorigin, self.xorigin) = origin
 
 
     def __eq__(self, other):
@@ -18,26 +18,27 @@ class Molecule():
             hasSameData = ~(~np.isclose(other.array, self.array)).sum()
             return hasSameData
         return False
+
     
     def data(self):
         return self.array
 
     def shape(self):
-        return (self.ny, self.nx, self.nz)
+        return (self.nz, self.ny, self.nx)
 
     def start_point(self):
-        return (self.nystart, self.nxstart, self.nzstart)
+        return (self.nzstart, self.nystart, self.nxstart)
 
     def grid_size(self):
-        return (self.my, self.mx, self.mz)
+        return (self.mz, self.my, self.mx)
 
     def cell_dim(self):
-        return (self.ylen, self.xlen, self.zlen)
+        return (self.zlen, self.ylen, self.xlen)
 
     def density_range(self):
         return (self.dmin, self.dmax, self.dmean)
 
     def origin(self):
-        return (self.yorigin, self.xorigin, self.zorigin)
+        return (self.zorigin, self.yorigin, self.xorigin)
 
 
