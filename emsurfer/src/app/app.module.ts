3edc2f19f324a4ecb6b36ae6d8b122a6900c36c6
JuViquez <jviquez2@gmail.com>
2019-03-18 16:13:36 -0600

    deleted home component from module

--- emsurfer/src/app/app.module.ts
+++ emsurfer/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { ContourShapeInputComponent } from './components/contour-shape-input/contour-shape-input.component';
 import { VolumeFilterInputComponent } from './components/volume-filter-input/volume-filter-input.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
-import { HomeComponent } from './components/home/home.component';
 import { EmdbIdInputComponent } from './components/emdb-id-input/emdb-id-input.component';
 import { UploadEmMapComponent } from './components/upload-em-map/upload-em-map.component';
 import { QueryMethodComponent } from './components/query-method/query-method.component';
@@ -26,7 +25,6 @@ import { ZernikeDescriptorsListComponent } from './components/zernike-descriptor
     ContourShapeInputComponent,
     VolumeFilterInputComponent,
     SearchFormComponent,
-    HomeComponent,
     EmdbIdInputComponent,
     UploadEmMapComponent,
     QueryMethodComponent,
