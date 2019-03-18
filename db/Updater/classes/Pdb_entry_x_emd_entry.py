ce2b737adf07f8fed8b495b325c9e4ca914833cd
LuisCastilloV98 <lui19982011@gmail.com>
2019-03-18 12:38:44 -0600

    Aggregation of Pdb_entry_x_emd_entry test

--- db/Updater/classes/Pdb_entry_x_emd_entry.py
+++ db/Updater/classes/Pdb_entry_x_emd_entry.py
@@ -3,6 +3,7 @@ Created on 22 feb. 2019
 
 @author: luis98
 '''
+from psycopg2 import sql
 
 class Pdb_entry_x_emd_entry(object):
     '''
@@ -14,6 +15,12 @@ class Pdb_entry_x_emd_entry(object):
     def __init__(self, pdb_entry_id, emd_entry_id):
         self.__pdb_entry_id = pdb_entry_id
         self.__emd_entry_id = emd_entry_id
+        
+        
+    def insert_db(self, cur):
+        cur.execute(sql.SQL("INSERT INTO pdb_entry_x_emd_entry(pdb_entry_id,emd_entry_id) VALUES (%s,%s);")
+        ,[self.__pdb_entry_id ,self.__emd_entry_id])
+
 
     def get_pdb_entry_id(self):
         return self.__pdb_entry_id
@@ -37,6 +44,11 @@ class Pdb_entry_x_emd_entry(object):
 
     def del_emd_entry_id(self):
         del self.__emd_entry_id
+        
+    
+    def __eq__(self, pdb_entry_x_emd_entry):        
+        return self.pdb_entry_id == pdb_entry_x_emd_entry.pdb_entry_id \
+           and self.emd_entry_id == pdb_entry_x_emd_entry.emd_entry_id
 
     pdb_entry_id = property(get_pdb_entry_id, set_pdb_entry_id, del_pdb_entry_id, "pdb_entry_id's docstring")
     emd_entry_id = property(get_emd_entry_id, set_emd_entry_id, del_emd_entry_id, "emd_entry_id's docstring")
