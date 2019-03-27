44a0549b2562608c42cf78d792d2e7900849c2cd
JuViquez <jviquez2@gmail.com>
2019-03-26 21:21:57 -0600

    test bechmark component

--- emsurfer/src/app/components/benchmark/benchmark.component.spec.ts
+++ emsurfer/src/app/components/benchmark/benchmark.component.spec.ts
@@ -30,33 +30,89 @@ class MockFileUploadService {
   }
 }
 
-describe("SearchFormComponent", () => {
-  let component: SearchFormComponent;
-  let fixture: ComponentFixture<SearchFormComponent>;
+describe("BenchmarkComponent", () => {
+  let component: BenchmarkComponent;
+  let fixture: ComponentFixture<BenchmarkComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, RouterTestingModule],
+      imports: [ReactiveFormsModule],
       declarations: [
-        SearchFormComponent,
-        HeaderComponent,
         ContourShapeInputComponent,
         VolumeFilterInputComponent,
-        EmdbIdInputComponent,
-        UploadEmMapComponent,
-        QueryMethodComponent,
-        ResolutionFilterComponent
+        EmdbIdListComponent,
+        UploadEmdbIdListFileComponent,
+        BenchmarkComponent
       ],
-      providers: [
-        { provide: Router, useClass: MockRouter },
-        { provide: FileUploadService, useClass: MockFileUploadService }
-      ]
+      providers: [{ provide: Router, useClass: MockRouter }]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SearchFormComponent);
+    fixture = TestBed.createComponent(BenchmarkComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  it("submit button should call submitHandler()", async(() => {
+    spyOn(component, "submitHandler");
+    const button = fixture.debugElement.nativeElement.querySelector(
+      "#submitButton"
+    );
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(component.submitHandler).toHaveBeenCalled();
+    });
+  }));
+
+  it("reset button should call reset", async(() => {
+    spyOn(component, "reset");
+    const button = fixture.debugElement.nativeElement.querySelector(
+      "#resetButton"
+    );
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(component.reset).toHaveBeenCalled();
+    });
+  }));
+
+  it("submitHandler should call router navigate with expected query params", inject(
+    [Router],
+    (router: Router) => {
+      spyOn(router, "navigate");
+      component.submitHandler();
+      const expectedQueryParams = {
+        emdbIdList: "1010,1884,5502",
+        contourRepresentation: 0,
+        volumeFilter: "On",
+        topResults: 20
+      };
+      const expectedUrl = "benchmark/results";
+      expect(router.navigate).toHaveBeenCalledWith([expectedUrl], {
+        queryParams: expectedQueryParams
+      });
+    }
+  ));
+
+  it("EMDB id list Radio buttons should call cbEmdbListChange() method", async(() => {
+    spyOn(component, "cbEmdbListChange");
+    const rbEmdb = fixture.debugElement.nativeElement.querySelector(
+      "#emdb-list"
+    );
+    rbEmdb.click();
+    fixture.whenStable().then(() => {
+      expect(component.cbEmdbListChange).toHaveBeenCalled();
+    });
+  }));
+
+  it("fileupload Radio buttons should call cbEmdbListChange() method", async(() => {
+    spyOn(component, "cbEmdbListChange");
+    const rbFileupload = fixture.debugElement.nativeElement.querySelector(
+      "#fileupload"
+    );
+    rbFileupload.click();
+    fixture.whenStable().then(() => {
+      expect(component.cbEmdbListChange).toHaveBeenCalled();
+    });
+  }));
 });
