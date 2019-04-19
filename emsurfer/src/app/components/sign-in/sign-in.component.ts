d131870e7db23115b6e5cb8246f611f922b2a135
JuViquez <jviquez2@gmail.com>
2019-04-19 15:20:20 -0600

    fixed login

--- emsurfer/src/app/components/sign-in/sign-in.component.ts
+++ emsurfer/src/app/components/sign-in/sign-in.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { AuthService } from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
 import { SocialUser } from "angularx-social-login";
 import { UserService } from "src/app/services/user.service";
-import { Credential } from "src/app/models/credential";
 
 @Component({
   selector: "app-sign-in",
   templateUrl: "./sign-in.component.html",
   styleUrls: ["bootstrap-social.css"]
 })
-export class SignInComponent implements OnInit {
-  private user: SocialUser;
+export class SignInComponent {
   private loggedIn: boolean;
 
   constructor(
@@ -19,24 +17,22 @@ export class SignInComponent implements OnInit {
     private userService: UserService
   ) {}
 
-  ngOnInit() {
-    this.authService.authState.subscribe(user => {
-      this.user = user;
-      this.loggedIn = user != null;
-    });
-  }
-
   signInWithGoogle(): void {
     this.authService
       .signIn(GoogleLoginProvider.PROVIDER_ID)
       .then((user: SocialUser) => {
-        this.userService.getAuthToken(user.idToken);
+        this.userService.getAuthToken(user.idToken).then(() => {
+          const credential = this.userService.getStoredAuthToken();
+          this.loggedIn = credential != null;
+        });
       });
   }
 
   signOut(): void {
     this.authService.signOut().then(() => {
       this.userService.deleteStoredAuthToken();
+      const credential = this.userService.getStoredAuthToken();
+      this.loggedIn = credential != null;
     });
   }
 }
