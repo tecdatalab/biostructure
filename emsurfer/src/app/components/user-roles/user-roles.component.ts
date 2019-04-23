13b9da114cf91b390658d679500b58e21f672645
joseant12 <joseantonio122@gmail.com>
2019-04-22 20:58:42 -0600

    Search by filter and pages implemented

--- emsurfer/src/app/components/user-roles/user-roles.component.ts
+++ emsurfer/src/app/components/user-roles/user-roles.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { User } from "src/app/models/user";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: "app-user-roles",
@@ -9,11 +10,15 @@ import { User } from "src/app/models/user";
 export class UserRolesComponent implements OnInit {
   constructor() {}
   users = [];
+  checkedOption;
+  currentPage = -1;
   filter;
   roles = [2, 0, 1];
+
   verify(user) {
     if (this.filter) {
-      if (user.id == this.filter) {
+      if (user[this.checkedOption].includes(this.filter)) {
+        this.currentPage = -1;
         return true;
       } else {
         return false;
@@ -23,13 +28,25 @@ export class UserRolesComponent implements OnInit {
   }
 
   createValues() {
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 150; i++) {
       this.users.push({
         id: i,
         name: "Name" + i,
         email: i + "@asdf.com",
         role: i % 2
       });
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage + 100 < this.users.length) {
+      this.currentPage += 100;
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage -= 100;
     }
   }
 
