b92bd02a422612ca9ddf1112a90e1dcd265a20db
Manuel Zumbado <manu3193@gmail.com>
2019-05-09 15:23:28 -0600

    add  high-res structure matching code

--- em/visualizer.py
+++ em/visualizer.py
@@ -19,7 +19,7 @@ class Visualizer():
 
                         attribute vec3      a_position;      // Vertex position
                         attribute vec3      a_normal;
-                        attribute vec3      u_color;
+                        attribute vec3      a_color;
 
                         varying vec3      v_color;
                         varying vec3      v_normal;
@@ -29,7 +29,7 @@ class Visualizer():
                         
                         void main()
                         {
-                            v_color = u_color;
+                            v_color = a_color;
                             v_normal   = a_normal;
                             v_position = a_position;
 
@@ -58,11 +58,11 @@ class Visualizer():
                             vec3 surfaceToLight = u_light_position - position;
                             // Calculate the cosine of the angle of incidence (brightness)
                             float brightness = dot(normal, surfaceToLight) / (length(surfaceToLight) * length(normal));
-                            brightness = max(min(brightness,1.0),0.0);
+                            brightness = max(min(brightness,1.0),0.25);
 
                             // Final color
-                            //gl_FragColor = vec4(v_color, 1.0) * (0.1 + 0.9*brightness * vec4(u_light_intensity, 1));
-                            gl_FragColor = vec4(v_color, 1.0);
+                            gl_FragColor = vec4(v_color, 1.0) * (0.1 + 0.9*brightness * vec4(u_light_intensity, 1));
+                            //gl_FragColor = vec4(v_color, 1.0);
                         } """
 
         self.labels = labels
@@ -83,7 +83,7 @@ class Visualizer():
             window.clear()
             gl.glDisable(gl.GL_BLEND)
             gl.glEnable(gl.GL_DEPTH_TEST)
-            gl.glEnable(gl.GL_POLYGON_OFFSET_FILL)
+            #gl.glEnable(gl.GL_POLYGON_OFFSET_FILL)
             
             points.draw(gl.GL_TRIANGLES, I)
 
@@ -116,16 +116,24 @@ class Visualizer():
         verts, faces, _,_ = measure.marching_cubes_lewiner(Z)
 
         verts_index = verts.astype(np.int32)
+        
 
         if self.labels.any() != None:
-            V = np.zeros(len(verts), [("a_position", np.float32, 3), ("u_color", np.float32, 3)])
-            colors = label2rgb(self.labels)
-            V["u_color"] = colors[verts_index[:,0], verts_index[:,1], verts_index[:,2]]
+            V = np.zeros(len(verts), [("a_position", np.float32, 3),  ("a_normal", np.float32, 3), ("a_color", np.float32, 3)])
+            colors = label2rgb(self.labels[verts_index[:,0], verts_index[:,1], verts_index[:,2]])
+            V["a_color"] = colors
         else:
-            V = np.zeros(len(verts), [("a_position", np.float32, 3)])
+            V = np.zeros(len(verts), [("a_position", np.float32, 3),  ("a_normal", np.float32, 3)])
         
         V["a_position"] = verts 
 
+        triangles = verts[faces]
+        n = np.cross( triangles[::,1 ] - triangles[::,0] , triangles[::,2 ] - triangles[::,0] )
+        n = n/np.linalg.norm(n, axis=1)[:,None]
+        V["a_normal"][ faces[:,0] ] += n 
+        V["a_normal"][ faces[:,1] ] += n 
+        V["a_normal"][ faces[:,2] ] += n 
+        V["a_normal"] = V["a_normal"] / np.linalg.norm(V["a_normal"], axis=1)[:,None]
    
 
         V = V.view(gloo.VertexBuffer)
@@ -142,43 +150,63 @@ class Visualizer():
         points['u_model'] = model
 
         view = np.eye(4, dtype=np.float32)
-        view = glm.translate(view, 0, 0, -dim[0]*4/5)
+        view = glm.translate(view, 0, 0, -dim[2]*4/5)
         points['u_view'] = view
 
-        points["u_light_position"] = -2,-2,2
+        points["u_light_position"] = 0,-1000,0
         points["u_light_intensity"] = 1,1,1
 
 
         app.run()
+        return verts
 
     def add_structure(self, filename):
         ppdb =PandasPdb()
         ppdb.read_pdb(filename)
         atoms = ppdb.df["ATOM"][ppdb.df['ATOM']['atom_name'] == 'CA']
-        self.atoms = atoms[["x_coord", "y_coord", "z_coord"]].values
+        self.atoms = atoms[["z_coord", "y_coord", "x_coord"]].values
         return self.atoms
 
 
 
-r = reader.Reader("../../EMD-1010.map")
+r = reader.Reader("../../EMD-1364.map")
 m = r.read()
+#cube = np.zeros((130,130,130))
+#cube[50:80,50:80,50:80] = 1
+#m.set_data(cube)
 import processing
 from skimage.measure import regionprops
-
+s = processing.gaussian_smooth(m,1)
 l = processing.watershed_segmentation(m)
 v = Visualizer(m,l)
 atoms_coords = v.add_structure("../../pdb1mi6.ent")
 zlen,ylen,xlen = m.cell_dim()
 mz, my, mx = m.grid_size()
+map_size = np.array([mz, my, mx])
 cell_dim = np.array([xlen,ylen,zlen])
-pixel_spacing = np.array([xlen/mx, ylen/my, zlen/mz]) 
-translation = np.array(m.shape())*pixel_spacing
-print(pixel_spacing)
+voxel_len = cell_dim/map_size
+print(voxel_len)
 print(cell_dim)
 
+
+verts =v.show()
+map_min = np.min(verts, axis=0)
+map_max = np.max(verts, axis=0)
+voxel_padding = map_size - (map_max-map_min)
+padding_adjustment = voxel_padding * voxel_len / 2
+print(padding_adjustment)
+pdb_min = np.min(atoms_coords, axis=0)
+adjustment = (pdb_min - padding_adjustment)/voxel_len
+print(adjustment)
+
+atoms_found=1
 for coord in atoms_coords:
     for region in regionprops(l):
-        if coord in region.coords:
-            print("Found atom with coords (%f, %f, %f) in region %d " % (coord[0], coord[1], coord[2], region.label))
+        min_z,min_y,min_x,max_z,max_y,max_x= region.bbox
+        min_box = np.array([min_z,min_y,min_x]) + adjustment
+        max_box = np.array([max_z,max_y,max_x]) + adjustment
+        if np.all(coord <= max_box) and np.all(coord >= min_box):
+            print("Found atom %d with coords (%f, %f, %f) in region %d " % (atoms_found, coord[0], coord[1], coord[2], region.label))
+            atoms_found+=1
 
-v.show()
+
