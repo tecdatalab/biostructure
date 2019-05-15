ec5b95f105884b53720159aed29b125b988b0996
Manuel Zumbado <manu3193@gmail.com>
2019-05-14 18:10:30 -0600

    add preliminar atom drawing

--- em/visualizer.py
+++ em/visualizer.py
@@ -1,162 +1,189 @@
 import numpy as np
 from glumpy import app, gloo, gl, glm
+from glumpy.transforms import Trackball, Position
 from skimage import measure
 from skimage.color import label2rgb
 from biopandas.pdb import PandasPdb 
 
 
-import molecule
 import reader
-
 
 class Visualizer():
     
     def __init__(self, myMolecule, labels=None):
         self.vertex = """
-                        uniform mat4      u_model;
-                        uniform mat4      u_view;       // View matrix
-                        uniform mat4      u_projection;   // Projection matrix
+                    uniform mat4 m_model;
+                    uniform mat4 m_view;
+                    uniform mat4 m_normal;
+                    attribute vec3 position;
+                    attribute vec3 normal;
+                    attribute vec3 color;
+                    varying vec3 v_normal;
+                    varying vec3 v_position;
+                    varying vec3 v_color;
+                    void main()
+                    {
 
-                        attribute vec3      a_position;      // Vertex position
-                        attribute vec3      a_normal;
-                        attribute vec3      a_color;
-
-                        varying vec3      v_color;
-                        varying vec3      v_normal;
-                        varying vec3      v_position;
-
-                        
-                        
-                        void main()
-                        {
-                            v_color = a_color;
-                            v_normal   = a_normal;
-                            v_position = a_position;
-
-                            gl_Position = u_projection * u_view * u_model * vec4(a_position,1.0);
-                        } """
+                        gl_Position = <transform>;
+                        vec4 P = m_view * m_model* vec4(position, 1.0);
+                        v_position = P.xyz / P.w;
+                        v_normal = vec3(m_normal * vec4(normal,0.0));
+                        v_color = color;
+                    }
+                    """
 
         self.fragment = """
-                        uniform mat4      u_model;           // Model matrix
-                        uniform mat4      u_view;            // View matrix
-                        uniform mat4      u_normal;          // Normal matrix
-                        uniform mat4      u_projection;      // Projection matrix
-                        uniform vec3      u_light_position;  // Light position
-                        uniform vec3      u_light_intensity; // Light intensity
-                        
-                        varying vec3      v_normal;          // Interpolated normal (in)
-                        varying vec3      v_position;        // Interpolated position (in)
-                        varying vec3      v_color;           // Interpolated color (in)
-
-                        void main()
+                    varying vec3 v_normal;
+                    varying vec3 v_position;
+                    varying vec3 v_color;
+                    const vec3 light_position = vec3(1.0,1.0,1.0);
+                    vec3 ambient_color = v_color;
+                    const vec3 diffuse_color = vec3(0.125, 0.125, 0.125);
+                    const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+                    const float shininess = 128.0;
+                    const float gamma = 2.2;
+                    void main()
+                    {
+                        vec3 normal= normalize(v_normal);
+                        vec3 light_direction = normalize(light_position + v_position);
+                        float lambertian = max(dot(light_direction,normal), 0.0);
+                        float specular = 0.0;
+                        if (lambertian > 0.0)
                         {
-                            // Calculate normal in world coordinates
-                            vec3 normal = normalize(u_normal * vec4(v_normal,1.0)).xyz;
-                            // Calculate the location of this fragment (pixel) in world coordinates
-                            vec3 position = vec3(u_view*u_model * vec4(v_position, 1.0));
-                            // Calculate the vector from this pixels surface to the light source
-                            vec3 surfaceToLight = u_light_position - position;
-                            // Calculate the cosine of the angle of incidence (brightness)
-                            float brightness = dot(normal, surfaceToLight) / (length(surfaceToLight) * length(normal));
-                            brightness = max(min(brightness,1.0),0.25);
-
-                            // Final color
-                            gl_FragColor = vec4(v_color, 1.0) * (0.1 + 0.9*brightness * vec4(u_light_intensity, 1));
-                            //gl_FragColor = vec4(v_color, 1.0);
-                        } """
-
+                            vec3 view_direction = normalize(v_position);
+                            vec3 half_direction = normalize(light_direction + view_direction);
+                            float specular_angle = max(dot(half_direction, normal), 0.0);
+                            specular = pow(specular_angle, shininess);
+                        }
+                        vec3 color_linear = ambient_color +
+                                            lambertian * diffuse_color +
+                                            specular * specular_color;
+                        vec3 color_gamma = pow(color_linear, vec3(1.0/gamma));
+                        gl_FragColor = vec4(color_gamma, 0.7);
+                    }
+                    """
         self.labels = labels
         self.molecule = myMolecule
         self.atoms = None
 
 
-
     def show(self):
-
-        window = app.Window(512, 512, color=(1,1,1,1))
-        phi, theta = 0, 0
-
-
-        @window.event
-        def on_draw(dt):
-            nonlocal phi, theta, model
-            window.clear()
-            gl.glDisable(gl.GL_BLEND)
-            gl.glEnable(gl.GL_DEPTH_TEST)
-            #gl.glEnable(gl.GL_POLYGON_OFFSET_FILL)
-            
-            points.draw(gl.GL_TRIANGLES, I)
-
-            glm.rotate(model, theta, 1, 0, 0)
-            glm.rotate(model, phi, 0, 1, 0)
-
-            phi = 0
-            theta = 0
-
-            view = points['u_view'].reshape(4,4)            
-            points['u_model'] = model
-            points['u_normal'] = np.array(np.matrix(np.dot(view, model)).I.T)
-
-
-        
-        @window.event
-        def on_resize(width, height):
-            points['u_projection'] = glm.perspective(45.0, width / float(height), 1.0, 256.0)
-
-        @window.event
-        def on_mouse_drag(x, y, dx, dy, buttons):
-            nonlocal phi, theta
-            phi += dx
-            theta += dy
-        
-            
-
-        Z = self.molecule.data()
-        # Marching cubes algorithm to get verts and faces to display
-        verts, faces, _,_ = measure.marching_cubes_lewiner(Z)
+        data = self.molecule.data()
+        verts, faces, _,_ = measure.marching_cubes_lewiner(data)
 
         verts_index = verts.astype(np.int32)
-        
 
         if self.labels.any() != None:
-            V = np.zeros(len(verts), [("a_position", np.float32, 3),  ("a_normal", np.float32, 3), ("a_color", np.float32, 3)])
+            V = np.zeros(len(verts), [("position", np.float32, 3),  ("normal", np.float32, 3), ("color", np.float32, 3)])
+            print(self.labels)
             colors = label2rgb(self.labels[verts_index[:,0], verts_index[:,1], verts_index[:,2]])
-            V["a_color"] = colors
+            V["color"] = colors * 0.5
         else:
-            V = np.zeros(len(verts), [("a_position", np.float32, 3),  ("a_normal", np.float32, 3)])
+            V = np.zeros(len(verts), [("position", np.float32, 3),  ("normal", np.float32, 3)])
         
-        V["a_position"] = verts 
+        T = verts[faces]
+        N = np.cross(T[::,1 ]-T[::,0], T[::,2]-T[::,0])
+        L = np.sqrt(N[:,0]**2+N[:,1]**2+N[:,2]**2)
+        N /= L[:, np.newaxis]
+        normals = np.zeros(verts.shape)
+        normals[faces[:,0]] += N
+        normals[faces[:,1]] += N
+        normals[faces[:,2]] += N
+        L = np.sqrt(normals[:,0]**2+normals[:,1]**2+normals[:,2]**2)
+        normals /= L[:, np.newaxis]
 
-        triangles = verts[faces]
-        n = np.cross( triangles[::,1 ] - triangles[::,0] , triangles[::,2 ] - triangles[::,0] )
-        n = n/np.linalg.norm(n, axis=1)[:,None]
-        V["a_normal"][ faces[:,0] ] += n 
-        V["a_normal"][ faces[:,1] ] += n 
-        V["a_normal"][ faces[:,2] ] += n 
-        V["a_normal"] = V["a_normal"] / np.linalg.norm(V["a_normal"], axis=1)[:,None]
-   
+        vmin, vmax =  verts.min(), verts.max()
+        verts_norm = 2*(verts-vmin)/(vmax-vmin) - 1
 
+        V["position"] = verts_norm
+        V["normal"] = normals / np.linalg.norm(normals, axis=1)[:,None]
+
+        ###ATOMS CODE
+        atoms_coords = self.add_structure("../../pdb1mi6.ent")
+        amin, amax =  atoms_coords.min(), atoms_coords.max()
+        atoms_norm = 2*(atoms_coords-amin)/(amax-amin) - 1
+        ####Generalte atoms vertices
+        
+        slices = 8 + 1
+        stacks = 8 + 1
+        n = slices*stacks
+        V_atoms = np.zeros(len(atoms_norm)*n, [("position", np.float32, 3),  ("normal", np.float32, 3), ("color", np.float32, 3)])
+        sphere_verts = np.zeros(n, [("position", np.float32, 3), ("normal", np.float32, 3), ("color", np.float32, 3)])
+        radius = 0.03
+        theta1 = np.repeat(np.linspace(0,     np.pi, stacks, endpoint=True), slices)
+        theta2 = np.tile(np.linspace(0, 2 * np.pi, slices, endpoint=True), stacks)
+        sphere_verts["position"][:,0] =  np.sin(theta1) * np.cos(theta2) * radius
+        sphere_verts["position"][:,1] =  np.cos(theta1) * radius
+        sphere_verts["position"][:,2] =  np.sin(theta1) * np.sin(theta2) * radius
+        print (sphere_verts.shape)
+        sphere_verts = np.repeat(sphere_verts[None,:], len(atoms_norm), axis=0).reshape(-1)
+        print (sphere_verts[0])
+        print (sphere_verts.shape)
+        print (atoms_norm.shape)
+        atoms_norm = np.repeat(atoms_norm, n, axis=0)
+        print (atoms_norm.shape)
+        V_atoms["position"][:,0] = atoms_norm[:,2] + sphere_verts["position"][:,2]
+        V_atoms["position"][:,1] = atoms_norm[:,1] + sphere_verts["position"][:,1]
+        V_atoms["position"][:,2] = atoms_norm[:,0] + sphere_verts["position"][:,0]
+        V_atoms["normal"] = V_atoms["position"]
+        V_atoms["color"] = np.repeat(np.array([[0.0, 0.1, 1.0]]),len(atoms_norm),axis=0)
+
+        vamin, vamax =  V_atoms["position"].min(), V_atoms["position"].max()
+        V_atoms["position"] = 2*(V_atoms["position"]-vamin)/(vamax-vamin) - 1
+        
+        V = np.concatenate((V, V_atoms), axis=0)
+
+        indices = []
+        initial_offset = len(verts)
+        for atom in range(len(atoms_coords)):
+            for i in range(stacks-1):
+                for j in range(slices-1):
+                    indices.append(i*(slices) + j + initial_offset)
+                    indices.append(i*(slices) + j+1 + initial_offset)
+                    indices.append(i*(slices) + j+slices+1 + initial_offset)
+                    indices.append(i*(slices) + j+slices + initial_offset)
+                    indices.append(i*(slices) + j+slices+1+ initial_offset)
+                    indices.append(i*(slices) + j+ initial_offset)
+            initial_offset+=n
+        indices = np.array(indices, dtype=np.uint32).reshape(-1,3)
+
+        I = np.concatenate(((faces).astype(np.uint32),indices), axis=0)
+        
+        
         V = V.view(gloo.VertexBuffer)
-        I = (faces).astype(np.uint32)
+        #I = (faces).astype(np.uint32)
         I = I.view(gloo.IndexBuffer)
-               
+
         points = gloo.Program(self.vertex, self.fragment)
         points.bind(V)
+        trackball = Trackball(Position("position"))
+        points['transform'] = trackball
+        trackball.theta, trackball.phi, trackball.zoom = 0, 0, 15
 
-        dim = self.molecule.shape()
+        window = app.Window(width=1024, height=1024, color=(1,1,1,1))
 
-        model = np.eye(4, dtype=np.float32)
-        model = glm.translate(model, -dim[0]/2)
-        points['u_model'] = model
+        def update():
+            model = points['transform']['model'].reshape(4,4)
+            view  = points['transform']['view'].reshape(4,4)
+            points['m_view']  = view
+            points['m_model'] = model
+            points['m_normal'] = np.array(np.matrix(np.dot(view, model)).I.T)
+            
+        @window.event
+        def on_draw(dt):
+            window.clear()
+            points.draw(gl.GL_TRIANGLES, I)
 
-        view = np.eye(4, dtype=np.float32)
-        view = glm.translate(view, 0, 0, -dim[2]*4/5)
-        points['u_view'] = view
+        @window.event
+        def on_mouse_drag(x, y, dx, dy, button):
+            update()
+            
+        @window.event
+        def on_init():
+            gl.glEnable(gl.GL_DEPTH_TEST)
+            update()
 
-        points["u_light_position"] = 0,-1000,0
-        points["u_light_intensity"] = 1,1,1
-
-
+        window.attach(points['transform'])
         app.run()
         return verts
 
@@ -169,16 +196,18 @@ class Visualizer():
 
 
 
-r = reader.Reader("../../EMD-1364.map")
+r = reader.Reader("../../EMD-1010.map")
 m = r.read()
 #cube = np.zeros((130,130,130))
 #cube[50:80,50:80,50:80] = 1
 #m.set_data(cube)
 import processing
 from skimage.measure import regionprops
-s = processing.gaussian_smooth(m,1)
-l = processing.watershed_segmentation(m)
+s = processing.gaussian_smooth(m,1.5)
+l = processing.watershed_segmentation(s)
 v = Visualizer(m,l)
+v.show()
+'''
 atoms_coords = v.add_structure("../../pdb1mi6.ent")
 zlen,ylen,xlen = m.cell_dim()
 mz, my, mx = m.grid_size()
@@ -208,5 +237,5 @@ for coord in atoms_coords:
         if np.all(coord <= max_box) and np.all(coord >= min_box):
             print("Found atom %d with coords (%f, %f, %f) in region %d " % (atoms_found, coord[0], coord[1], coord[2], region.label))
             atoms_found+=1
-
+'''
 
