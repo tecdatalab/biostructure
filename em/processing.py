89c923bb87bb90111f68d0fc4b55e01b848f6c1a
manu3193 <manu3193@gmail.com>
2019-05-18 21:37:30 -0600

    update watershed marker generation

--- em/processing.py
+++ em/processing.py
@@ -1,22 +1,28 @@
 from scipy import ndimage as ndi
-from skimage.morphology import watershed, local_maxima
+from skimage.morphology import watershed, erosion, dilation
 from skimage.feature import peak_local_max
 from skimage.filters import threshold_otsu, gaussian
 from skimage.color import label2rgb
 from copy import copy
 import numpy as np
 
+import matplotlib
+matplotlib.use('Qt4Agg')
+import matplotlib.pyplot as plt
 
 
 
 def watershed_segmentation(myMolecule):
     imageData = myMolecule.data()
     th = threshold_otsu(imageData)
-    binary = imageData > th
-    distance = ndi.distance_transform_edt(binary)
-    local_max = peak_local_max(distance, indices=False, labels=binary, footprint=np.ones((26,26,26)), exclude_border=1)
-    markers = ndi.label(local_max, structure=np.ones((3,3,3)))[0] 
-    labels = watershed(-distance,markers,mask=binary)
+    thresholded = imageData > th
+    kernel = np.ones((3,3,3))
+    eroded = erosion(thresholded, kernel)
+    dilated = dilation(eroded, kernel)   
+    distance = ndi.distance_transform_edt(dilated)
+    distance =  distance/np.linalg.norm(distance.ravel(), np.inf, keepdims=True)
+    markers,num = ndi.label(np.where(distance>0.7, 1.0, 0.0), structure=kernel)
+    labels = watershed(-distance, markers)
     return labels
 
 def gaussian_smooth(myMolecule, sigma = 1):
