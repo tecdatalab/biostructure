6c2bea0171bd1ee5e6146f4f36b324fe21ec4ba3
joseant12 <joseantonio122@gmail.com>
2019-05-20 18:30:59 -0600

    Search button

--- emsurfer/src/app/components/search-history/search-history.component.ts
+++ emsurfer/src/app/components/search-history/search-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { SearchHistory } from "src/app/models/search-history";
 import { SearchHistoryService } from "src/app/services/search-history.service";
 
@@ -8,8 +9,25 @@ import { SearchHistoryService } from "src/app/services/search-history.service";
   styleUrls: ["./search-history.component.css"]
 })
 export class SearchHistoryComponent implements OnInit {
-  constructor(private searchHistoryService: SearchHistoryService) {}
+  constructor(
+    private searchHistoryService: SearchHistoryService,
+    private router: Router
+  ) {}
   records: SearchHistory[];
+
+  searchByEmdbID(record) {
+    console.log(record);
+    const url = "result/" + record.emd_entry_id;
+    const params = {
+      contourRepresentation: record.representation_id,
+      volumeFilter: record.volume_filter_id,
+      minResolution: record.resolution_filter_min,
+      maxResolution: record.resolution_filter_max
+    };
+    this.router.navigate([url], {
+      queryParams: params
+    });
+  }
 
   ngOnInit() {
     this.searchHistoryService
