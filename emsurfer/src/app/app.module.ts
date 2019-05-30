136d2f44d63056948bf400bd50f8925c2dc7d0d3
JuViquez <jviquez2@gmail.com>
2019-05-29 19:10:09 -0600

    tutorial page

--- emsurfer/src/app/app.module.ts
+++ emsurfer/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { ZernikeDescriptorsModuleComponent } from "./components/zernike-descript
 import { ZernikeResultComponent } from "./components/zernike-result/zernike-result.component";
 import { UserRolesComponent } from "./components/user-roles/user-roles.component";
 import { StatisticsTableComponent } from './components/statistics-table/statistics-table.component';
+import { TutorialPageComponent } from './components/tutorial-page/tutorial-page.component';
 
 @NgModule({
   declarations: [
@@ -59,7 +60,8 @@ import { StatisticsTableComponent } from './components/statistics-table/statisti
     ZernikeDescriptorsModuleComponent,
     ZernikeResultComponent,
     UserRolesComponent,
-    StatisticsTableComponent
+    StatisticsTableComponent,
+    TutorialPageComponent
   ],
   imports: [
     BrowserModule,
