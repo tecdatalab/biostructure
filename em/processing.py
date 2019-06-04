fd34cc45cc8870b8fbca260c75ae17a16800cb99
manu3193 <manu3193@gmail.com>
2019-06-04 03:23:00 -0600

    extend watershed to group regions by space-scale filter

--- em/processing.py
+++ em/processing.py
@@ -3,37 +3,41 @@ from skimage.morphology import dilation, ball
 from skimage.segmentation import watershed
 from skimage.feature import peak_local_max
 from skimage.filters import  gaussian
+from skimage.measure import regionprops
 from copy import deepcopy
 import numpy as np
 
 
 
-def watershed_segmentation(myMolecule, level):
+def watershed_segmentation(myMolecule, level, scale_maps, steps):
     imageData = myMolecule.data()
     thresholded = imageData > level
     mask = dilation(thresholded, ball(2))
-
-
-    
-    #-------
-    import matplotlib
-    matplotlib.use('TkAgg')
-    import matplotlib.pyplot as plt
-    #------
-    
     labels = watershed(-imageData, connectivity=26, mask=mask)
+    local_maxima = peak_local_max(imageData, labels=labels, num_peaks_per_label=1)
+
+    for step in range(steps):
+        step_local_maxima = peak_local_max(scale_maps[step].data())
+        for point in np.nditer(local_maxima):
+            print("Local maxima ", len(local_maxima))
+            print(np.linalg.norm(step_local_maxima-point, axis=1).argmin(axis=0))
+            local_maxima[i] = np.linalg.norm(step_local_maxima-point, axis=1).argmin(axis=0)
+    print(local_maxima)
+
+
     
-    plt.imshow(imageData[48], cmap='gray')
-    plt.show()
-    plt.imshow(mask[48], cmap='gray')
-    plt.show()
-    plt.imshow(labels[48])
-    plt.show()
-    
+    #Unify regions
+
+    #atoms_distance["distance"] = distance(atomic_structure["position"])
     return labels
 
-def gaussian_smooth(myMolecule, sigma = 1):
-    newMolecule = deepcopy(myMolecule)
-    smoothed = gaussian(newMolecule.data(), sigma=sigma)
-    newMolecule.set_data(smoothed)
-    return newMolecule
+def gaussian_step_filtering(myMolecule, step_sigma, steps):
+    step_maps = []
+    initial_sigma = step_sigma
+    for map_id in range(steps):
+        newMolecule = deepcopy(myMolecule)
+        smoothed = gaussian(newMolecule.data(), sigma=initial_sigma)
+        newMolecule.set_data(smoothed)
+        step_maps.append(newMolecule)
+        initial_sigma+=step_sigma
+    return step_maps
