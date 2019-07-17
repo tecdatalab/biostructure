6bb5697e20bacc32bbd8fb5d4c87bdc59bf63be2
LuisCastilloV98 <lui19982011@gmail.com>
2019-07-16 20:52:47 -0600

    Aggregation of the final EMD of the execution.

--- db/Updater/connections_tests/FTP_connection_test.py
+++ db/Updater/connections_tests/FTP_connection_test.py
@@ -19,19 +19,19 @@ def test_0_get_all_emds_id(ftpserver):
     ftp_connection = FTP_connection("localhost", ftpserver.server_port)     
     copy_files(ftpserver)
     ftp_connection.init_connection("structures")
-    result = ftp_connection.get_all_emds_id("0001")
+    result = ftp_connection.get_all_emds_id("0001","inf")
     assert result == ["0001"]
 
 def test_1_get_all_emds_id(ftpserver):
     ftp_connection = FTP_connection("localhost", ftpserver.server_port)     
     copy_files(ftpserver)
     ftp_connection.init_connection("structures")
-    result = ftp_connection.get_emds_higher_than_date(datetime.datetime(2000,2,14),"0001")
+    result = ftp_connection.get_emds_higher_than_date(datetime.datetime(2000,2,14),"0001","inf")
     assert result == ["0001"]
 
 def test_2_get_all_emds_id(ftpserver):
     ftp_connection = FTP_connection("localhost", ftpserver.server_port)     
     copy_files(ftpserver)
     ftp_connection.init_connection("structures")
-    result = ftp_connection.get_emds_higher_than_date(datetime.datetime(2020,2,14),"0001")
+    result = ftp_connection.get_emds_higher_than_date(datetime.datetime(2020,2,14),"0001","inf")
     assert result == []
