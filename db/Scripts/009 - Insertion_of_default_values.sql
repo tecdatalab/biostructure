19ba72fdc7cb51bedd272380ee12a00d329912ec
LuisCastilloV98 <lui19982011@gmail.com>
2019-07-29 21:49:14 -0600

    Repare some errors in the normal execution

--- db/Scripts/009 - Insertion_of_default_values.sql
+++ db/Scripts/009 - Insertion_of_default_values.sql
@@ -15,9 +15,8 @@ INSERT INTO user_role VALUES ('1', 'User'), ('2', 'Admin');
 
 INSERT INTO volume_filter VALUES ('0','Off'), ('1','On');
 
-INSERT INTO type_descriptor VALUES (1, "EMDB Contour", "EMDB Contour"),
-(2, "EMDB Contour + 1/3 core", "EMDB Contour + 1/3 core"),
-(3, "EMDB Contour + 2/3 core", "EMDB Contour + 2/3 core"),
-(4, "EMDB Contour + 1/3 + 2/3 core", "EMDB Contour + 1/3 + 2/3 core"),
-(5, "EMDB Contour + 1 std dev", "EMDB Contour + 1 std dev");
-
+INSERT INTO type_descriptor VALUES (1,'EMDB Contour', 'EMDB Contour'),
+(2, 'EMDB Contour + 1/3 core', 'EMDB Contour + 1/3 core'),
+(3, 'EMDB Contour + 2/3 core', 'EMDB Contour + 2/3 core'),
+(4, 'MDB Contour + 1/3 + 2/3 core', 'EMDB Contour + 1/3 + 2/3 core'),
+(5, 'EMDB Contour + 1 std dev', 'EMDB Contour + 1 std dev');
