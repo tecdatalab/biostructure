03ce04fb67cc4c64474348275d451b4115b631e7
manu3193 <manu3193@gmail.com>
2019-08-19 01:39:48 -0600

    remove level ratio print and comment example use

--- em/visualizer.py
+++ em/visualizer.py
@@ -74,7 +74,7 @@ class Visualizer():
         
         if level!=None:
             self.th_level = level
-            print("Using density threshold level: %.4f, ratio: %f" % (self.th_level, level))
+            print("Using density threshold level: %.4f" % (self.th_level))
         else:
             self.th_level =  threshold_otsu(data)
             print("Using density threshold level: %.4f" % (self.th_level))
@@ -376,22 +376,23 @@ class Visualizer():
 
 
 #Read molecule map from file
-mapReader = reader.Reader()
+#mapReader = reader.Reader()
 #Open file
-#mapReader.open("../maps/EMD-2596.map")
-#mapReader.open("../maps/1010/EMD-1010.map")
-mapReader.open("../maps/1364/EMD-1364.map")
-#mapReader.open("../maps/5017/EMD-5017.map")
-#mapReader.open("emd_8750.map")
+#mapReader.open("../maps/EMD-1048.map") #level 2.2479
+#mapReader.open("../maps/EMD-2596.map") #level 0.1462
+#mapReader.open("../maps/1010/EMD-1010.map") #level 23
+#mapReader.open("../maps/1364/EMD-1364.map") #level 39.0086
+#mapReader.open("../maps/5017/EMD-5017.map") #level 17.3347
+
 
 #Get map object
-myMap = mapReader.read()
+#myMap = mapReader.read()
 # Create visualizer with a map surface threshold level
 # Otherwise use otsu threshold
-v= Visualizer(myMap, level=23)
+#v= Visualizer(myMap, level=17.3347)
 #v = Visualizer(myMap)
 # Watershed 
-v.segmentation(step_sigma=1, steps=0)
+#v.segmentation(step_sigma=1, steps=3)
 
 # add corresponding atomic structure
 #v.add_structure("../maps/1010/pdb1mi6.ent")
@@ -413,7 +414,7 @@ v.segmentation(step_sigma=1, steps=0)
 #v.map_structure_to_domain("../maps/5017/C-1N0U.aligned.pdb")
 
 #v.show_atom_matching()
-v.show()
+#v.show()
 
 
 
