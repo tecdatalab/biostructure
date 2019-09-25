80175a15fa3b7557267845b5e22e29683b853b5e
Esteban Sanabria <24924856+Esteasv31@users.noreply.github.com>
2019-09-25 11:52:26 -0600

    Update FTP connection get_all_emds_id loop

--- db/Updater/connections/FTP_connection.py
+++ db/Updater/connections/FTP_connection.py
@@ -47,24 +47,24 @@ class FTP_connection(object):
         result = []
         #elements = [172,174,306,307,436,616,1508,1571,1653,1662,1823,1824,1826,1906,2061,2062,2101,2272,2273,2362,2363,2432,2489,2610,2641,2708,3036,3094,3095,3122,3771,3792,3820,3821,3868,4065,4263,4376,4377,4419,4421,4422,4433,4445,4446,4447,4461,4483,4484,4486,4490,4491,4492,4633,5003,5160,5384,5485,5486,5538,5539,5582,5584,5678,5695,5696,5724,5728,5754,5764,5947,5992,6034,6035,6037,6038,6148,6213,6214,6371,6374,6375,6376,6377,6407,6444,6471,6472,6473,6619,6620,6737,6846,6854,6855,6907,6914,6948,6968,6969,6976,7034,7047,7110,7116,7117,7125,7126,7135,7136,7138,7139,7140,7141,7142,7143,7144,7145,7146,7147,7148,7150,7151,7152,7153,7154,7315,7316,7317,7472,7543,7589,7599,7623,7624,7625,7627,7628,7629,7630,8116,8147,8157,8304,8314,8320,8328,8332,8333,8334,8344,8471,8485,8606,8655,8656,8661,8689,8703,8733,8734,8753,8754,8755,8760,8761,8762,8763,8846,8854,9012,9022,9198,9199,9200,9260,9261,9366,9367,9564,9565,9665,9666,9667,9864,9865,9866,9867,9868,9869,9879,9920,9921,20061,20067,20068,20091,20195,20197,20199,20206]
         self.__ftp.retrlines("NLST",files.append)
-        # for i in range(len(files)):
-        #     #if int(files[i].replace("EMD-", "")) in elements:
-        #     #    continue
-        #     if int(files[i].replace("EMD-", ""))<int(initialEMD):
-        #         continue
-        #     if int(files[i].replace("EMD-", ""))>float(finalEMD):
-        #         break
-        #     try:
-        #         self.__ftp.voidcmd("MDTM EMD-{0}/map/emd_{0}.map.gz".format(files[i].replace("EMD-", "")))
-        #         result.append(files[i].replace("EMD-", ""))
-        #     except:
-        #         pass
-        for i in range(0,40):
+        for i in range(len(files)):
+            #if int(files[i].replace("EMD-", "")) in elements:
+            #    continue
+            if int(files[i].replace("EMD-", ""))<int(initialEMD):
+                continue
+            if int(files[i].replace("EMD-", ""))>float(finalEMD):
+                break
             try:
                 self.__ftp.voidcmd("MDTM EMD-{0}/map/emd_{0}.map.gz".format(files[i].replace("EMD-", "")))
                 result.append(files[i].replace("EMD-", ""))
             except:
                 pass
+        '''for i in range(0,4):
+            try:
+                self.__ftp.voidcmd("MDTM EMD-{0}/map/emd_{0}.map.gz".format(files[i].replace("EMD-", "")))
+                result.append(files[i].replace("EMD-", ""))
+            except:
+                pass'''
         result.sort(key = self.sortKey)
         return result
     
