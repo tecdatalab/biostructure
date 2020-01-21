ac5414b0add3f75a8f4743bf272c6893406b8241
manu3193 <manu3193@gmail.com>
2020-01-21 00:25:07 -0500

    created main program

--- em/main.py
+++ em/main.py
@@ -1,48 +1,72 @@
 
 import reader
-import processing
 import visualizer
+import processing
+import model
 
-#Read molecule map from file
-#mapReader = reader.Reader()
-#Open file
-#mapReader.open("../maps/1010/EMD-1010.map") #level 2.2479
-# #mapReader.open("../maps/EMD-2596.map") #level 0.1462
-#mapReader.open("../maps/1010/EMD-1010.map") #level 23
-#mapReader.open("../maps/1364/EMD-1364.map") #level 39.0086
-#mapReader.open("../maps/5017/EMD-5017.map") #level 17.3347
+import configparser
+import argparse
 
 
-#Get map object
-#myMap = mapReader.read()
-# Create visualizer with a map surface threshold level
-# Otherwise use otsu threshold
-#v= Visualizer(myMap, level=9.07)
-#v = Visualizer(myMap)
-# Watershed 
-#v.segmentation(step_sigma=1, steps=3)
 
-# add corresponding atomic structure
-#v.add_structure("../maps/1010/pdb1mi6.ent")
-#v.map_structure_to_domain("../maps/1010/A-1GQE.aligned.pdb")
-#v.map_structure_to_domain("../maps/1010/B-1GQE.aligned.pdb")
-#v.map_structure_to_domain("../maps/1010/C-1GQE.aligned.pdb")
-#v.map_structure_to_domain("../maps/1010/D-1GQE.aligned.pdb")
 
-#v.add_structure("../maps/1364/pdb1pn6.ent")
-#v.map_structure_to_domain("../maps/1364/A-1FNM.aligned.pdb")
-#v.map_structure_to_domain("../maps/1364/B-1FNM.aligned.pdb")
-#v.map_structure_to_domain("../maps/1364/C-1FNM.aligned.pdb")
-#v.map_structure_to_domain("../maps/1364/D-1FNM.aligned.pdb")
-#v.map_structure_to_domain("../maps/1364/E-1FNM.aligned.pdb")
+def main():
+    #config = configparser.ConfigParser()
+    #parser = argparse.ArgumentParser(description='Molecule Visualization and segmentation tool')
+    
+    #Read molecule map from file
+    mapReader = reader.Reader()
+    #Open file
+    mapReader.open("../maps/EMD-9590.map") #level 2.2479
+    # #mapReader.open("../maps/EMD-2596.map") #level 0.1462
+    #mapReader.open("../maps/1010/EMD-1010.map") #level 23
+    #mapReader.open("../maps/1364/EMD-1364.map") #level 39.0086
+    #mapReader.open("../maps/5017/EMD-5017.map") #level 17.3347
 
-#v.add_structure("../maps/5017/pdb3dny.ent")
-#v.map_structure_to_domain("../maps/5017/A-1N0U.aligned.pdb")
-#v.map_structure_to_domain("../maps/5017/B-1N0U.aligned.pdb")
-#v.map_structure_to_domain("../maps/5017/C-1N0U.aligned.pdb")
 
-#v.show_atom_matching()
-#v.show()
+    #Get map object
+    myMap = mapReader.read()
+    # Create visualizer with a map surface threshold level
+    # Otherwise use otsu threshold
+    #v= visualizer.Visualizer(myMap, level=0.05)
+    #
+    # Watershed 
+    contourRatioLvl = [1,0.75,0.5]
+    myModel = model.Model(myMap, 0.05, contourRatioLvl)
+    labels, data = processing.segmentate(myModel, step_sigma=1, steps=5)
+    #v.add_structure("../maps/pdb6acf.ent")
+    #v.map_structure_to_domain("../maps/pdb6acf.ent")
+    countourLvl = myModel.getCutoffLevels()
+    v1 = visualizer.Visualizer(data[0], countourLvl[0], labels[0])
+    v2 = visualizer.Visualizer(data[1], countourLvl[1], labels[1])
+    v3 = visualizer.Visualizer(data[2], countourLvl[2], labels[2])
+
+    v1.show()
+    v2.show()
+    v3.show()
+
+
+    # add corresponding atomic structure
+    #v.add_structure("../maps/1010/pdb1mi6.ent")
+    #v.map_structure_to_domain("../maps/1010/A-1GQE.aligned.pdb")
+    #v.map_structure_to_domain("../maps/1010/B-1GQE.aligned.pdb")
+    #v.map_structure_to_domain("../maps/1010/C-1GQE.aligned.pdb")
+    #v.map_structure_to_domain("../maps/1010/D-1GQE.aligned.pdb")
+
+    #v.add_structure("../maps/1364/pdb1pn6.ent")
+    #v.map_structure_to_domain("../maps/1364/A-1FNM.aligned.pdb")
+    #v.map_structure_to_domain("../maps/1364/B-1FNM.aligned.pdb")
+    #v.map_structure_to_domain("../maps/1364/C-1FNM.aligned.pdb")
+    #v.map_structure_to_domain("../maps/1364/D-1FNM.aligned.pdb")
+    #v.map_structure_to_domain("../maps/1364/E-1FNM.aligned.pdb")
+
+    #v.add_structure("../maps/5017/pdb3dny.ent")
+    #v.map_structure_to_domain("../maps/5017/A-1N0U.aligned.pdb")
+    #v.map_structure_to_domain("../maps/5017/B-1N0U.aligned.pdb")
+    #v.map_structure_to_domain("../maps/5017/C-1N0U.aligned.pdb")
+
+    #v.show_atom_matching()
+    #v.show()
 
 
 
