00a0596a539e1631070f20a892d4abb620d0c7c8
manu3193 <manu3193@gmail.com>
2020-01-23 16:41:09 -0500

    add model methods to get molecule properties

--- em/model.py
+++ em/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage import regionprops
 
 class Model():
     def __init__(self,molecule, recommendedContour, cutoffRatios=[1], segments=None, atoms=None):
@@ -6,18 +7,31 @@ class Model():
         self.contourLvl=recommendedContour
         self.cutoffRatios=cutoffRatios
         self.contoursNum= len(cutoffRatios)
+        self.data_
         self.segments=[]
         self.atoms=atoms
 
     def getData(self):
         moleculeData = self.molecule.data()
         mydata = np.ndarray((self.contoursNum,*moleculeData.shape))
+        
         for i,cutoffRatio in enumerate(self.cutoffRatios):
             data = np.copy(moleculeData)
             data[data<cutoffRatio*self.contourLvl]=0
             mydata[i,:] = data
-        return mydata
+        return mydata   
 
     def getCutoffLevels(self):
         return [cutoffRatio*self.contourLvl for cutoffRatio in self.cutoffRatios]
 
+    def getGridSize(self):
+        return self.molecule.grid_size()
+
+    def getCellDim(self):
+        return self.molecule.cell_dim()
+
+    def getSegments(self):
+        return self.labels,self.segments
+
+    #def setSegmentLabels(self, labels):
+    #    regions = regionprops(labels,this.)
