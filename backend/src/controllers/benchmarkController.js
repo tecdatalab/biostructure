9145aa78099c30b9153d6588d953ade876a28add
Esteban <24924856+Esteasv31@users.noreply.github.com>
2020-05-15 16:25:42 -0600

    Benchmark Fix for local development

--- backend/src/controllers/benchmarkController.js
+++ backend/src/controllers/benchmarkController.js
@@ -6,9 +6,8 @@ const zipFolder = require("zip-a-folder");
 exports.batchQuery = async (req, res, next) => {
   try {
     const list = req.params.emdblist.split(",");
-    benchmark_history
+    await benchmark_history
       .build({
-        id:0,
         date_time: new Date(),
         ip: req.connection.remoteAddress,
         user_id: null,
@@ -20,8 +19,8 @@ exports.batchQuery = async (req, res, next) => {
       .save()
       .then(newBenchmark => {
         const benchmarkPath = "./public/benchmarks/" + newBenchmark.id;
-        mkdirp(benchmarkPath + "/results", function(err) {
-          generateFiles(list, benchmarkPath, newBenchmark.id, function(
+        mkdirp(benchmarkPath + "/results", function (err) {
+          generateFiles(list, benchmarkPath, newBenchmark.id, function (
             response
           ) {
             if (Array.isArray(response)) {
@@ -39,6 +38,9 @@ exports.batchQuery = async (req, res, next) => {
             }
           });
         });
+      })
+      .catch(function (err) {
+        console.log(err, req.body);
       });
   } catch (err) {
     res.status(500).send({
@@ -52,13 +54,13 @@ function generateFiles(IDList, benchmarkPath, idFolder, callback) {
     "Rank	EMDB_ID	EUC_D	RESOLUTION \r\n 1	6409	12.851	22 \r\n 2	4804	12.945	14 \r\n 3	6478	15.250	35 \r\n 4	9618	19.548	10.6 \r\n";
   let filesPaths = [];
   let file;
-  IDList.forEach(function(ID, index, array) {
+  IDList.forEach(function (ID, index, array) {
     /* Iterate over each ID */
     file = {
       filename: "EMDB-" + ID + ".hit",
       path: benchmarkPath + "/results/EMDB-" + ID + ".hit"
     };
-    fs.writeFile(file.path, text, function(err) {
+    fs.writeFile(file.path, text, function (err) {
       if (err) {
         callback(err);
       }
@@ -70,7 +72,7 @@ function generateFiles(IDList, benchmarkPath, idFolder, callback) {
       zipFolder.zipFolder(
         benchmarkPath + "/results",
         benchmarkPath + "/results.zip",
-        function(err) {
+        function (err) {
           if (err) {
             callback(err);
           } else {
