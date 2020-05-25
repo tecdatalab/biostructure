46734048c1ff46d5c3d74fff55d797ea522c1688
Esteban <24924856+Esteasv31@users.noreply.github.com>
2020-05-25 16:12:19 -0600

    Base template for visualizer component

---
+++ emsurfer/src/app/components/biomolecule-3d-visualizer/biomolecule-3d-visualizer.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-biomolecule-3d-visualizer',
+  templateUrl: './biomolecule-3d-visualizer.html',
+  styleUrls: ['./biomolecule-3d-visualizer.css']
+})
+export class BiomoleculeVizualizerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
