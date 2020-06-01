d5afa4ecb1983fe2abe8b8f457591113ef2e30b7
Esteban Mata <esteban1113001@gmail.com>
2020-06-01 14:42:25 -0600

    Added a working stage for NGL Viewer

--- emsurfer/src/app/components/biomolecule-3d-visualizer/biomolecule-3d-visualizer.component.ts
+++ emsurfer/src/app/components/biomolecule-3d-visualizer/biomolecule-3d-visualizer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as NGL from "ngl/dist/ngl.esm.js";
 
 @Component({
-  selector: 'app-biomolecule-3d-visualizer',
-  templateUrl: './biomolecule-3d-visualizer.component.html',
-  styleUrls: ['./biomolecule-3d-visualizer.component.css']
+  selector: "app-biomolecule-3d-visualizer",
+  templateUrl: "./biomolecule-3d-visualizer.component.html",
+  styleUrls: ["./biomolecule-3d-visualizer.component.css"],
 })
-
 export class BiomoleculeVizualizerComponent implements OnInit {
+  stage = null;
+  viewer = null;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() { }
-
+  ngOnInit() {
+    this.stage = new NGL.Stage("viewport", {
+      theme: "dark",
+    });
+    this.stage.loadFile(
+      "http://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-0001/map/emd_0001.map.gz",
+      { defaultRepresentation: true }
+    );
+  }
 }
