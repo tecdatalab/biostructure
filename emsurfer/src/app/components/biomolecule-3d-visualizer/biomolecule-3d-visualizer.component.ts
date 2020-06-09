be41e99abeb6e29cfaa21a8d298878ae3ad0920e
Esteban <24924856+Esteasv31@users.noreply.github.com>
2020-06-09 17:04:42 -0600

    add code for different examples

--- emsurfer/src/app/components/biomolecule-3d-visualizer/biomolecule-3d-visualizer.component.ts
+++ emsurfer/src/app/components/biomolecule-3d-visualizer/biomolecule-3d-visualizer.component.ts
@@ -12,12 +12,29 @@ export class BiomoleculeVizualizerComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    
     this.stage = new NGL.Stage("viewport", {
       theme: "dark",
     });
+
+    var schemeId = NGL.ColormakerRegistry.addSelectionScheme([
+      ["red", "64-74 or 134-154 or 222-254 or 310-310 or 322-326"],
+      ["green", "311-322"],
+      ["yellow", "40-63 or 75-95 or 112-133 or 155-173 or 202-221 or 255-277 or 289-309"],
+      ["blue", "1-39 or 96-112 or 174-201 or 278-288"],
+      ["white", "*"]
+    ], "Transmembrane 3dqb");
+
     this.stage.loadFile(
-      "http://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-0001/map/emd_0001.map.gz",
+      //   "http://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-0001/map/emd_0001.map.gz",
+      // "../../../assets/example-files/emd_1884.map",
+      //   "../../../assets/example-files/emd_0001.map.gz",
+      "../../../assets/example-files/1crn.pdb",
       { defaultRepresentation: true }
-    );
+    ).then(function(o){
+      o.addRepresentation("cartoon", {color: schemeId });
+      o.autoView();
+    });
+
   }
 }
