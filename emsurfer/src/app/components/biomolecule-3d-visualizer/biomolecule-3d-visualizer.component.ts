d7192345d42004e952305bccc08723fec8c90102
Esteban Mata <esteban1113001@gmail.com>
2020-07-03 16:08:09 -0600

    spheres on random points being added

--- emsurfer/src/app/components/biomolecule-3d-visualizer/biomolecule-3d-visualizer.component.ts
+++ emsurfer/src/app/components/biomolecule-3d-visualizer/biomolecule-3d-visualizer.component.ts
@@ -12,29 +12,50 @@ export class BiomoleculeVizualizerComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    
     this.stage = new NGL.Stage("viewport", {
       theme: "dark",
     });
 
-    var schemeId = NGL.ColormakerRegistry.addSelectionScheme([
-      ["red", "64-74 or 134-154 or 222-254 or 310-310 or 322-326"],
-      ["green", "311-322"],
-      ["yellow", "40-63 or 75-95 or 112-133 or 155-173 or 202-221 or 255-277 or 289-309"],
-      ["blue", "1-39 or 96-112 or 174-201 or 278-288"],
-      ["white", "*"]
-    ], "Transmembrane 3dqb");
+    let schemeId = NGL.ColormakerRegistry.addSelectionScheme(
+      [
+        ["red", "64-74 or 134-154 or 222-254 or 310-310 or 322-326"],
+        ["green", "311-322"],
+        [
+          "yellow",
+          "40-63 or 75-95 or 112-133 or 155-173 or 202-221 or 255-277 or 289-309",
+        ],
+        ["blue", "1-39 or 96-112 or 174-201 or 278-288"],
+        ["white", "*"],
+      ],
+      "Transmembrane 3dqb"
+    );
 
     this.stage.loadFile(
       //   "http://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-0001/map/emd_0001.map.gz",
       // "../../../assets/example-files/emd_1884.map",
       //   "../../../assets/example-files/emd_0001.map.gz",
-      "../../../assets/example-files/1crn.pdb",
+      "../../../assets/example-files/emd_1884.map",
       { defaultRepresentation: true }
-    ).then(function(o){
-      o.addRepresentation("cartoon", {color: schemeId });
-      o.autoView();
-    });
+    );
 
+    this.stage.signals.clicked.add(this.click);
+  }
+
+  private click(PickingProxy: NGL.pickingProxy) {
+    console.log(PickingProxy);
+    if (PickingProxy.mouse.shiftKey) {
+      const pos = document.getElementById("text") as HTMLElement;
+      pos.textContent = `x:${PickingProxy.mouse.canvasPosition.x}, y:${PickingProxy.mouse.canvasPosition.y}`;
+
+      const shape = new NGL.Shape("shape", { disableImpostor: true });
+      shape.addSphere(
+        Array.from({ length: 3 }, () => Math.floor(Math.random() * 600 - 300)),
+        Array.from({ length: 3 }, () => Math.floor(Math.random() * 250)),
+        30,
+        "la bolita"
+      );
+      const nosequesesto = PickingProxy.stage.addComponentFromObject(shape);
+      nosequesesto.addRepresentation("buffer");
+    }
   }
 }
