ed8775bf31991be59f693fee8eafe977ea71d614
manu3193 <manu3193@gmail.com>
2020-07-17 18:03:41 -0600

    fix bug in contour mask compute for each contour level

--- em/molecule.py
+++ em/molecule.py
@@ -34,7 +34,7 @@ class Molecule():
         for i,cutoffRatio in enumerate(self.cutoffRatios):
             lvl = cutoffRatio*self.contourLvl
             mask_at_contour = np.zeros(map_data.shape)
-            mask_at_contour[mask_at_contour<=lvl]=self.indicatorValue
+            mask_at_contour[map_data<=lvl]=self.indicatorValue
             contour_masks[i,:] = mask_at_contour
 
         self.contour_masks = contour_masks
@@ -94,4 +94,9 @@ class Molecule():
                 pass
             else:
                 self.contour_maks[i,:] = self.data[i].ascontiguousarray('C') 
+
+
+
+
+
                 
