efedd8073b20728696a44aec886bb08eb0e3573b
manu3193 <manu3193@gmail.com>
2020-07-24 12:21:40 -0600

    fix missing parenthesis

--- em/emMap.py
+++ em/emMap.py
@@ -13,7 +13,7 @@ class EMMap():
         (self.zorigin, self.yorigin, self.xorigin) = origin
         (self.dmin, self.dmax, self.dmean) = density_ranges
         self.axis_order = axis_order
-        self.voxel_size = tuple(i/j) for i,j in zip(self.cell_dim(), self.grid_size()))
+        self.voxel_size = (tuple(i/j) for i,j in zip(self.cell_dim(), self.grid_size()))
         
 
 
