d668970548674d7d90beec9beeffc0e241b4d44d
dnnxl <dnnxl15@gmail.com>
2020-08-07 18:28:01 -0600

    Update write method

--- db/Updater/generators/writer.py
+++ db/Updater/generators/writer.py
@@ -1,18 +1,22 @@
-import numpy as np
+import numpy as np 
 import struct
+'''
 
+Last modification on 7 aug. 2020
+Author: dnnxl
+'''
 
 class Writer():
 
-    # Header size in bytes
+    #Header size in bytes
     HEADER_SIZE = 1024
 
-    # Write density map as ".map" format. Default write mode is set to mode 2.
-    def write(self, filename, molecule):
-        # First generate buffer from array data in mode 2
-        data = bytearray()
-        for voxel in np.nditer(molecule.data()):
-            data += struct.pack('f', voxel)
+    # Write density map as ".map" format. Default write mode is set to mode 2. 
+    def write(self, filename, molecule, data):
+        #First generate buffer from array data in mode 2
+        dataOut = bytearray()
+        for voxel in np.nditer(data):
+            dataOut+=struct.pack('f',voxel)        
         try:
             with open(filename, 'wb') as output:
                 header = bytearray(molecule.rawHeader)
@@ -20,16 +24,14 @@ class Writer():
                 header[196:208] = struct.pack('<fff', *molecule.origin())
 
                 output.write(header)
-                output.write(data)
+                output.write(dataOut)
         except Exception as err:
             print("Could not write file, error ", err)
 
-
-'''
-import reader
-filename = "tests/EMD-2677.map"
-myreader = reader.Reader(filename)
-D = myreader.read()
-w = Writer()
-w.write("emd_2677_2.map", D)
-'''
+#import reader
+#filename = "../maps/1010/EMD-1010.map"
+#myreader = reader.Reader() 
+#myreader.open(filename)
+#D = myreader.read()
+#w = Writer()
+#w.write("emd_2677_2.map", D, D.data())
