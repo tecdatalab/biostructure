d092347e1f9597a137694b8fd519594f60d41d76
dnnxl <dnnxl15@gmail.com>
2020-08-07 18:42:29 -0600

    Refactor constant and add algorithm constant

--- db/Updater/constants/constants.py
+++ db/Updater/constants/constants.py
@@ -6,68 +6,88 @@
  This file will keep all the static 
  values of the files.
 '''
-
 import enum 
 
-# class TypeErrorEmd enum
-# Description: The following class contain all the app that will be using this file.
-# By: Danny Xie Li
-# Created: 16/05/2020
-# Last modification: 16/05/2020
-
 class App(enum.Enum):
+    '''
+    The App enum deal with the app.
+
+    Enum values:
+        updater - Updater
+    '''
     updater = "Updater" # Represent the application updater.
 
-# class TypeErrorEmd enum
-# Description: The following class contain all the message of error of the main_emd.py for the logging file.
-# By: Danny Xie Li
-# Created: 16/05/2020
-# Last modification: 06/08/2020
-
 class TypeErrorEmd(enum.Enum):
-    DFE = "DFE" # Not a error, default value.
-    EDG = "Error in the images generation of EMD {0}" # Error en la generación del descriptor EMD id.
-    ena = "ENA" # An error occurred (Computed a NaN!).
-    ECS = "Error in the descriptor generation of EMD {0}.Countour level or std not exist." # Contour level or std not exist.
-    EIN = "Error in the insertion of EMD {0}" # Error in the insertion.
-    EID = "Error in the descriptor generation of EMD {0}" # Error in the descriptor generation.
-    ETS = "Error in the time stamp generation of EMD {0}" # Error in the time stamp generation.
-    EGS = "Error in the generation of segments EMD {0}" # Error in the generation of segments.
-    ECE = "Error in the calculation of the volume of the EMD {0}" # Error in the calculation of the volume of the EMD.
-    ECV = "Error in the calculation of the volume of the segments in EMD {0}" # Error in the calculation of the volume of the segments in EMD.
-    EIS = "Error in the insertion of segments in EMD {0}" # Error in the insertion of segments in EMD.
+    '''
+    The TypeErrorEmd enum deal with the type of errors of emd in the updater.
 
-# class TypeErrorPdb enum
-# Description: The following class contain all the message of error of the main_pdb.py for the logging file.
-# By: Danny Xie Li
-# Created: 16/05/2020
-# Last modification: 16/05/2020
+    Enum values:
+        DFE - DFE, not a error, default value.
+        EDG - Error en la generación del descriptor EMD id.
+        ENA - An error occurred (Computed a NaN!).
+        ECS - Contour level or std not exist.
+        EIN - Error in the insertion of emd.
+        EID - Error in the descriptor generation.
+        ETS - Error in the time stamp generation.
+        EGS - Error in the generation of segments.
+        ECE - Error in the calculation of the volume of the EMD.
+    '''
+    DFE = "DFE" 
+    EDG = "Error in the images generation of EMD {0}" 
+    ena = "ENA" 
+    ECS = "Error in the descriptor generation of EMD {0}.Countour level or std not exist." 
+    EIN = "Error in the insertion of EMD {0}" 
+    EID = "Error in the descriptor generation of EMD {0}" 
+    ETS = "Error in the time stamp generation of EMD {0}" 
+    EGS = "Error in the generation of segments EMD {0}" 
+    ECE = "Error in the calculation of the volume of the EMD {0}"
 
 class TypeErrorPdb(enum.Enum):
-    EEP = "Error in execution {0} with PDB {1}" # Error in execution in PDB.
-    ECC = "Error in execution cath complex {0} with complex {1}" # Error in cath complex.
-    ECH = "Error in execution cath chain {0} with chain {1}" # Error in the execution cath chain.
-    ECD = "Error in execution cath domain {0} with domain {1}" # Error in exeution cath domain.
-    EAS = "Error in execution atomic structure x emd_entry {0} with atomic_structure {1}, emd_entry {2}" # Error in execution atomic structure.
+    '''
+    The TypeErrorPdb enum deal with the type of errors of pdb in the updater.
 
-# class TypeMessage enum
-# Description: The following class contain all the message of the logging file.
-# By: Danny Xie Li
-# Created: 16/05/2020
-# Last modification: 16/05/2020
+    Enum values:
+        EEP - Error in execution in PDB.
+        ECC - Error in cath complex.
+        ECH - Error in the execution cath chain.
+        ECD - Error in exeution cath domain.
+        EAS - Error in execution atomic structure.
+    '''
+    EEP = "Error in execution {0} with PDB {1}" 
+    ECC = "Error in execution cath complex {0} with complex {1}" 
+    ECH = "Error in execution cath chain {0} with chain {1}" 
+    ECD = "Error in execution cath domain {0} with domain {1}"
+    EAS = "Error in execution atomic structure x emd_entry {0} with atomic_structure {1}, emd_entry {2}" 
 
 class TypeMessage(enum.Enum):
+    '''
+    The TypeMessage enum deal with the type of messages in the updater.
+
+    Enum values:
+        MS1 - Message of the execution start.
+        MET - Message of execution time.
+        MS2 - Message of execution finished.
+    '''
     MS1 = "------------------------------The execution start ------------------------------"
     MET = "Execution time: {0}"
     MS2 = "---------------------------- The execution finished -----------------------------"
 
-# class Constant
-# Description: This class contain of the constants of the file
-# By: Danny Xie Li
-# Created: 16/05/2020
-# Last modification: 16/05/2020
-
 class Constant:
+    '''
+    The Constant class contained some constants used in the updater.
+
+    Constant values:
+        formatLog - Format log.
+        config    - Configuration format.
+    '''
     formatLog = '[%(asctime)s] %(app)s %(file)s [%(type)s] %(message)s  %(error)s'
     config = {'app': App.updater.value, 'file':__file__, 'type': TypeErrorEmd.DFE.value, 'error':''}
 
+class Algorithm_segmentation(enum.Enum):
+    '''
+    The Algorithm_segmentation enum deal with the algorithm to generate the segments.
+
+    Enum values:
+        DEFAULT - default
+    '''
+    DEFAULT = 'default'
