033e008c25499bab5710b0a83569097f7f4f3f24
Manuel Zumbado <manu3193@gmail.com>
2021-05-27 14:29:57 -0600

    accept input as directory path to be able to process several maps

--- em/evolutionary_segmentation/main.py
+++ em/evolutionary_segmentation/main.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import pickle 
 import copy
-
+import glob
 
 MAX_NUMBER_SEGMENTS=60
 
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     #   --n_max max number of generations
     '''
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_path', required=True, help= 'Input mrc file path')
+    parser.add_argument('--input_path', required=True, help= 'Input mrc directory path')
     parser.add_argument('--output_path', required=True, help= 'Output directory path')
     parser.add_argument('--level', required=True, help= 'Isosurface level')
     #parser.add_argument('--label_path', required=True, help='Input mrc file path with labels')
@@ -47,59 +47,57 @@ if __name__ == '__main__':
         os.mkdir(opt.output_path)
     print('init')
     classifier = pickle.load(open('classifier.pkl', 'rb'))
-    print(opt.input_path)
-    mol = molecule.Molecule(opt.input_path,float(opt.level))
-    #mol_gt = molecule.Molecule(opt.label_path, 0.01)
-    data = mol.getDataAtContour(1)
-    #data_gt = mol_gt.getDataAtContour(1)
-    #data[data_gt==0]=0
-    mol.setData(data)
-   
-    pop = evo_seg.init_population(int(opt.init_size), MAX_NUMBER_SEGMENTS, [1,4], [1,4], mol)
-    print("Init population score:")
-    for p in pop:
-        print(classifier.predict_proba(p['features'].reshape(1, -1)))
-
-    save=False
-    counter = 0
-    run= True
-    pop_fitness = None
-    overall_best_score = 0
-    patience = int(opt.n_patience)
-    test_id = os.path.basename(opt.input_path).split('.')[0]
-    
-    score_list = []
-    score_segmentation_list = []
-    
-    while(run):
-        ma, pa = evo_seg.select_parents(pop, int(opt.n_mates))
-        new_gen = evo_seg.mating(ma, pa, float(opt.p_mates), 0.5, data)
-        mutated_pop = evo_seg.mutate(new_gen, float(opt.p_split), float(opt.p_merge), [1,4], [1,4], mol)
-        pop_fitness = [ classifier.predict_proba(n['features'].reshape(1, -1))[0][1] for n in mutated_pop ]
-        print("Population of gen {} fitness {}".format(counter,pop_fitness))
-        best_individual_score = np.max(pop_fitness)
-        std_score_pop = np.std(pop_fitness)
-   
-        print("***Optimizing {}, best score of generation {} is {}".format(test_id, counter, best_individual_score))
-        print("         population std of {}".format(std_score_pop))
-        save = True if best_individual_score > overall_best_score else False
-        sorted_idx = np.argsort(pop_fitness)
-        overall_best_score = best_individual_score if best_individual_score > overall_best_score else overall_best_score
-        score_list.append(best_individual_score)
-        print('computing matching IoU..')
-        current_segmentation = copy.deepcopy(mol)
-        mol.setData(mutated_pop[sorted_idx[-1]]['labels'])
-        score_segmentation_list.append(matching_iou(mol_gt,current_segmentation))
-        if save:
-            print("     saving segmentation...")
-            save_path = os.path.join(output_path,'best_{0}_{1:.2f}.mrc'.format(test_id,best_individual_score))
-            mol.save(save_path)
-            patience = int(opt.n_patience)
-        else:
-            patience -= 1
+    print("Loading maps from {}".format(opt.input_path))
+    filenames = glob.glob(opt.input_path)
+    for f in filenames:
+        mol = molecule.Molecule(f,0.01)
+        #mol_gt = molecule.Molecule(opt.label_path, 0.01)
+        data = mol.getDataAtContour(1) 
+        #data_gt = mol_gt.getDataAtContour(1)
+        #data[data_gt==0]=0
+        mol.setData(data)
+       
+        pop = evo_seg.init_population(30, MAX_NUMBER_SEGMENTS, [1,4], [1,4], mol)
+        save=False
+        counter = 0
+        run= True
+        pop_fitness = None
+        overall_best_score = 0
+        patience = 30 #int(opt.n_patience)
+        test_id = os.path.basename(opt.input_path).split('.')[0]
         
-        run = False if ((counter > int(opt.n_max)) | (patience==0)) else True
-        counter += 1
+        score_list = []
+        score_segmentation_list = []
+        top_5 = [] 
+        while(run):
+            ma, pa = evo_seg.select_parents(pop, 25) #int(opt.n_mates))
+            new_gen = evo_seg.mating(ma, pa, 0.5, 0.5, data)
+            mutated_pop = evo_seg.mutate(new_gen, 0.01, 0.01, [1,4], [1,4], mol)
+            pop_fitness = [ classifier.predict_proba(n['features'].reshape(1, -1))[0][1] for n in new_gen ]
+            sorted_idx = np.argsort(pop_fitness)
+            if len(top_5) > 0:
+                for t in top_5:
+                    mutated_pop.append(t)
+            top_5 = [ copy.deepcopy(new_gen[sorted_idx[i]]) for i in range(-5,0)  ]
+            pop_fitness = [ classifier.predict_proba(n['features'].reshape(1, -1))[0][1] for n in mutated_pop ]
+            sorted_idx = np.argsort(pop_fitness)
+            print("Population of gen {} fitness {}".format(counter,pop_fitness))
+            best_individual_score = np.max(pop_fitness)
+            print("***Optimizing {}, best score of generation {} is {}".format(test_id, counter, best_individual_score))
+            save = True if best_individual_score > overall_best_score else False
+            overall_best_score = best_individual_score if best_individual_score > overall_best_score else overall_best_score
+            score_list.append(best_individual_score)
+            current_segmentation = mutated_pop[sorted_idx[-1]]['labels']
+            if save:
+                print("     saving segmentation...")
+                save_path = os.path.join(opt.output_path,'best_{0}_{1:.2f}.npy'.format(test_id,best_individual_score))
+                np.save(save_path, current_segmentation)
+                patience = 30
+            else:
+                patience -= 1
+            pop = mutated_pop            
+            run = False if ((counter >= 200) | (patience==0)) else True
+            counter += 1
 
-    # plot results
-    plot_evolutionary_graph(score_list, score_segmentation_list, os.path.join(opt.output_path,'evolutionary_result'), 'Matching IoU') 
+        # plot results
+        #plot_evolutionary_graph(score_list, score_segmentation_list, os.path.join(opt.output_path,'evolutionary_result'), 'Matching IoU') 
