e34303be0b70e0231e3a184be091ec640a2075ab
Luis José Castillo Valverde <lui19982011@gmail.com>
2021-06-22 15:30:12 -0600

    Change forme to do pdb_percentage

--- reconstruction/experiment/utils_general.py
+++ reconstruction/experiment/utils_general.py
@@ -14,87 +14,19 @@ from general_utils.pdb_utils import get_chains_pdb, get_cube_pdb, move_pdb_cente
 from general_utils.temp_utils import gen_dir, free_dir
 
 
-def get_parallel_can_chains_chunk(pdb_name, dir_path):
-  from general_utils.pdb_utils import get_chains_pdb
-
-  try:
-    path_of_pdb = '{0}/{1}.pdb'.format(dir_path, pdb_name)
-    download_pdb(pdb_name, path_of_pdb)
-    chains = get_chains_pdb(path_of_pdb)
-  except:
-    path_of_cif = '{0}/{1}.cif'.format(dir_path, pdb_name)
-    download_cif(pdb_name, path_of_cif)
-    chains = get_chains_cif(path_of_cif)
-    path_of_pdb = os.path.join(dir_path, "pdbFile.pdb")
-    cif_to_pdb(path_of_cif, path_of_pdb)
-    os.remove(path_of_cif)
-
-  move_pdb_center(path_of_pdb)
-  cube_dimensions = get_cube_pdb(path_of_pdb)
-
-  result = [pdb_name, len(chains), cube_dimensions]
-  os.remove(path_of_pdb)
-  return result
-
-
 def pdb_percentage(percentage, executor=None, min_can_chains=0):
-  headers_csv = ['Pdb', 'Can Chains', 'Cube dimension']
-
-  # List know can chains pdb
-  know_can_chains_pdb_path = os.path.dirname(__file__) + '/../files/pdb_can_chains.csv'
-
-  if os.path.exists(know_can_chains_pdb_path):
-    pd_data_frame = pd.read_csv(know_can_chains_pdb_path,
-                                converters={"Cube dimension": literal_eval})
-    can_chains_list_name = pd_data_frame["Pdb"].tolist()
-  else:
-    can_chains_list_name = []
+  from general_utils.database_utils import get_chains_pdb_db
 
   # Process
-  all_names = get_all_pdb_work()  # 169315
-  # all_names = all_names[:3]
-  all_names = np.setdiff1d(np.array(all_names), np.array(can_chains_list_name))
-  all_names = all_names.tolist()
-
-  # Add chains
-  dirpath = gen_dir()
-
-  flag_error = False
-
-  if executor is not None:
-    parallel_jobs = []
-    for i in all_names:
-      parallel_jobs.append([executor.submit(get_parallel_can_chains_chunk, i, dirpath)])
-
-    for f in parallel_jobs:
-      try:
-        list_append = f[0].result()
-        data_write = [list_append]
-        write_in_file(know_can_chains_pdb_path, headers_csv, data_write)
-      except Exception as e:
-        flag_error = True
-  else:
-    for i in all_names:
-      list_append = get_parallel_can_chains_chunk(i, dirpath)
-      data_write = [list_append]
-      write_in_file(know_can_chains_pdb_path, headers_csv, data_write)
-
-  free_dir(dirpath)
-
-  if flag_error:
-    raise NameError('Error in download pdbs')
-
-  # Gen data use
-  pd_data_frame = pd.read_csv(know_can_chains_pdb_path,
-                              converters={"Cube dimension": literal_eval})
   can_chains_list_dic = {}
-
-  for value in pd_data_frame.values.tolist():
-    exist_flag = can_chains_list_dic.get(value[1], None)
+  all_names = get_all_pdb_work()  # 169315
+  for pdb_name in all_names:
+    chains_len = get_chains_pdb_db(pdb_name)
+    exist_flag = can_chains_list_dic.get(chains_len, None)
     if exist_flag == None:
-      can_chains_list_dic[value[1]] = [value[0]]
+      can_chains_list_dic[chains_len] = [pdb_name]
     else:
-      can_chains_list_dic[value[1]].append(value[0])
+      can_chains_list_dic[chains_len].append(pdb_name)
 
   # Gen result
   result = []
