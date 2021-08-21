6f91faaec6dafdd3c6d1e60f67bb64a7118bb3b2
Luis José Castillo Valverde <lui19982011@gmail.com>
2021-08-20 21:55:31 -0600

    Add dowloand all pdbs

--- reconstruction/general_utils/download_utils.py
+++ reconstruction/general_utils/download_utils.py
@@ -100,7 +100,7 @@ def download_emd_xml(id_code, exit_path, create_progress_bar=False):
 
 
 def download_pdb(id_code, exit_path, create_progress_bar=False):
-  possible_local_path = "/work/lcastillov/allPDBS/"
+  possible_local_path = "/work/lcastillov/allPDBS_unzip/"
 
   if os.path.exists(possible_local_path):
     possible_pdb_path = os.path.join(possible_local_path, "{}.pdb".format(possible_local_path))
@@ -139,7 +139,7 @@ def download_pdb_online(id_code, exit_path, create_progress_bar=False):
 
 
 def download_cif(id_code, exit_path, create_progress_bar=False):
-  possible_local_path = "/work/lcastillov/allCIFS/"
+  possible_local_path = "/work/lcastillov/allCIFS_unzip/"
 
   if os.path.exists(possible_local_path):
     possible_cif_path = os.path.join(possible_local_path, "{}.cif".format(possible_local_path))
--- reconstruction/main/all_CIFBank.py
+++ reconstruction/main/all_CIFBank.py
@@ -10,21 +10,21 @@ from tqdm import tqdm
 from general_utils.terminal_utils import get_out
 
 
-def downloadModelsPDB():
-  get_out("rsync -rlpt --ignore-existing -v -z --delete --port=33444 rsync.rcsb.org::ftp_data/structures/divided/pdb/ /work/lcastillo/allPDBS_tmp/")
 
-  list_dirs = glob.glob("/work/lcastillo/allPDBS_tmp/*.ent.gz")
+def downloadModelsCIF():
+  get_out("rsync -rlpt --ignore-existing -v -z --delete --port=33444 rsync.rcsb.org::ftp_data/structures/divided/mmCIF/ /work/lcastillo/allCIFS_tmp/")
+
+  list_dirs = glob.glob("/work/lcastillo/allCIFS_tmp/*.cif.gz")
 
   with tqdm(total=len(list_dirs)) as pbar:
     for file in list_dirs:
       base_name = os.path.basename(file)
       base_name = base_name.split(".")[0]
-      base_name = base_name[3:]
 
-      final_path = "/work/lcastillo/allPDBS_unzip/{}.pdb".format(base_name)
+      final_path = "/work/lcastillo/allCIFS_unzip/{}.cif".format(base_name)
 
       actual_file = os.path.dirname(file)
-      actual_file = os.path.join(actual_file, 'pdb{0}.ent'.format(base_name))
+      actual_file = os.path.join(actual_file, 'pdb{0}.cif'.format(base_name))
 
       get_out("gunzip", "--force", file)
       get_out("mv", actual_file, final_path)
@@ -32,6 +32,5 @@ def downloadModelsPDB():
       pbar.update(1)
 
 
-
-print("PDB")
-downloadModelsPDB()
+print("CIF")
+downloadModelsCIF()
