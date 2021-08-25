cb624b6882251a0b8b9c6c6e20fcb5d4c49e4c93
Luis José Castillo Valverde <lui19982011@gmail.com>
2021-08-25 09:40:11 -0600

    Add dowloand all pdbs

--- reconstruction/main/main_experiment1_b_RMSD1.py
+++ reconstruction/main/main_experiment1_b_RMSD1.py
@@ -90,8 +90,13 @@ def experiment_1_b_RMSD_chain():
             if total_updated != 0 and total_updated % 1000 == 0:
               print("Star save process", flush=True)
               csv_file["RMSD"] = new_RMSD_list_update
-              csv_file.to_csv(temp_csv_v1, index=False)
+              print("1", flush=True)
+              timer = time.time()
+              csv_file.to_csv(temp_csv_v1, chunksize=100000, index=False)
+              timerf = time.time()
+              print("2", timerf - timer, flush=True)
               os.remove(temp_csv)
+              print("3", flush=True)
               os.rename(temp_csv_v1, temp_csv)
               print("Finish save process", flush=True)
 
@@ -178,8 +183,13 @@ def experiment_1_b_RMSD_struct():
           if total_updated != 0 and total_updated % 1000 == 0:
             print("Star save process", flush=True)
             csv_file["RMSD"] = new_RMSD_list_update
-            csv_file.to_csv(temp_csv_v1, index=False)
+            print("1", flush=True)
+            timer = time.time()
+            csv_file.to_csv(temp_csv_v1, chunksize=100000, index=False)
+            timerf = time.time()
+            print("2", timerf - timer, flush=True)
             os.remove(temp_csv)
+            print("3", flush=True)
             os.rename(temp_csv_v1, temp_csv)
             print("Finish save process", flush=True)
 
