86fe1e5a71678bbafab587a1d2d39fd38c10352e
Manuel Zumbado <manu3193@gmail.com>
2022-11-04 21:44:33 -0400

    use device in Dataset, import train_test_split

--- em/src/deep_segmentation/extreme_points/SegmentationAgent.py
+++ em/src/deep_segmentation/extreme_points/SegmentationAgent.py
@@ -9,9 +9,7 @@ from SegmentationDataset import SegmentationDataset
 from SegmentationUNet import SegmentationUNet
 from CustomLoss import CustomLoss
 
-from em.molecule import Molecule
-from scipy.ndimage import zoom
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
 
 import pandas as pd
 
@@ -23,7 +21,6 @@ def set_seed(seed):
     np.random.seed(seed)
 
 def get_optimizer(name, parameters, learning_rate, momentum, weight_decay):
-    print(name)
     if name=='SGD':
         return SGD(parameters, lr=learning_rate,momentum=momentum, weight_decay=weight_decay)
     elif name=='Adam':
@@ -58,17 +55,17 @@ class SegmentationAgent:
             idist.barrier()
 
         train_split, val_split, test_split = self.make_splits(num_folds, test_ids, seed)
-        train_dataset = SegmentationDataset(train_split, self.num_classes, self.img_size, self.randg, augmentate=True, extra_width)
-        validation_dataset = SegmentationDataset(val_split, self.num_classes, self.img_size, self.randg, augmentate=False, extra_width)
-        test_dataset = SegmentationDataset(test_split, self.num_classes, self.img_size, self.randg, augmentate=False, extra_width)
+        train_dataset = SegmentationDataset(train_split, self.num_classes, self.img_size, self.randg, self.device, augmentate=True, extra_width=extra_width)
+        validation_dataset = SegmentationDataset(val_split, self.num_classes, self.img_size, self.randg, self.device, augmentate=False, extra_width=extra_width, is_validation=True)
+        test_dataset = SegmentationDataset(test_split, self.num_classes, self.img_size, self.randg, self.device, augmentate=False, extra_width=extra_width, is_validation=True)
         if idist.get_local_rank() == 0:
             idist.barrier()
 
-        self.train_loader = idist.auto_dataloader(train_dataset, batch_size=batch_size, shuffle=True, worker_init_fn=np.random.seed(self.seed))
-        self.validation_loader = idist.auto_dataloader(train_dataset, batch_size=batch_size, shuffle=False, worker_init_fn=np.random.seed(self.seed))
-        self.test_loader = DataLoader(test_dataset, batch_size=2*batch_size, shuffle=False, worker_init_fn=np.random.seed(self.seed), pin_memory=True)
+        self.train_loader = idist.auto_dataloader(train_dataset, batch_size=self.batch_size, shuffle=True, worker_init_fn=np.random.seed(self.seed))
+        self.validation_loader = idist.auto_dataloader(train_dataset, batch_size=self.batch_size, shuffle=False, worker_init_fn=np.random.seed(self.seed))
+        self.test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, worker_init_fn=np.random.seed(self.seed), pin_memory=True)
 
-        self.model = idist.auto_model(SegmentationUNet(self.num_classes, self.device, depth=self.depth))
+        self.model = idist.auto_model(SegmentationUNet(self.num_classes, self.device, in_channels=2, depth=self.depth))
         self.criterion = CustomLoss(loss_func, self.num_classes, self.device, self.alpha, self.beta,  mixed_precision)
         self.optimizer = idist.auto_optim(get_optimizer(optimizer, self.model.parameters(), learning_rate, momentum, weight_decay))
         
@@ -83,12 +80,17 @@ class SegmentationAgent:
         :return: tuples of splits
         """
         # Get id list of EM maps in data
-        id_list = self.dataframe.groupby('id')['id'].unique().index.tolist()
+        df_subunits = self.dataframe.drop_duplicates(subset=['id','subunit'])
+        id_list = df_subunits.index.tolist()
         # Generate folds for cross validation
         splits = KFold(n_splits=num_folds,shuffle=True,random_state=seed)
         # Select testing maps from dataset
-        test_maps = [test_id for test_id in test_ids if test_id in id_list ]
-        samples = [sample_id for sample_id in id_list if sample_id not in test_maps ] 
+        if len(test_ids)>0:
+            test_maps = [test_id for test_id in test_ids if test_id in id_list ]
+            samples = [sample_id for sample_id in id_list if sample_id not in test_maps ]
+        else:
+            samples, test_maps = train_test_split(id_list, test_size=0.2, random_state=seed) 
+            
         # Generate folds for cross validation
         splits = KFold(n_splits=num_folds,shuffle=True,random_state=seed)
         train_idx = []
@@ -98,26 +100,14 @@ class SegmentationAgent:
             val_idx.append(v)
         validation_maps = [ samples[i] for i in val_idx[self.current_fold] ] 
         train_maps = [ samples[i] for i in train_idx[self.current_fold] ]
-        print("Number of EM Maps in total: {}, Training: {}, Validation: {}, Testing: {}".format(len(id_list), len(train_maps), len(validation_maps), len(test_maps)))
+        print("Number of samples in total: {}, Training: {}, Validation: {}, Testing: {}".format(len(id_list), len(train_maps), len(validation_maps), len(test_maps)))
         validation_segments = self.dataframe[self.dataframe['id'].isin(validation_maps)]
         train_segments = self.dataframe[self.dataframe['id'].isin(train_maps)]
         test_segments = self.dataframe[self.dataframe['id'].isin(test_maps)].drop_duplicates(subset=['id','subunit'])
-        print("Number of Segments in total: Training: {}, Validation: {}, Testing: {}".format(len(train_segments.drop_duplicates(subset=['id','subunit'])), len(validation_segments.drop_duplicates(subset=['id','subunit'])), len(test_segments)))
+        #print("Number of Segments in total: Training: {}, Validation: {}, Testing: {}".format(len(train_segments.drop_duplicates(subset=['id','subunit'])), len(validation_segments.drop_duplicates(subset=['id','subunit'])), len(test_segments)))
         print("Training samples: {}".format(train_maps))
         print("Validation samples: {}".format(validation_maps))
         print("Testing samples: {}".format(test_maps))
         return train_segments, validation_segments, test_segments
 
-    def get_dataloader(self, split, extra_width=0, split_name=None, shuffle=False):
-        """
-        Create a DataLoader for the given split
-        :param split: train split, validation split or test split of the data
-        :return: DataLoader
-        """
-        dataset = SegmentationDataset(split, self.num_classes, self.img_size, self.randg, self.device, extra_width)
-        if split_name == 'test':
-            batch_size = len(dataset)
-        else:
-            batch_size = self.batch_size
-        return DataLoader(dataset, batch_size, shuffle=shuffle, worker_init_fn=np.random.seed(self.seed))
 
