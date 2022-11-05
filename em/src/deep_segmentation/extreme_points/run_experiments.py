29bebe064d2c294ebab204c4daa435d7b3dd9a45
Manuel Zumbado <manu3193@gmail.com>
2022-11-04 22:03:34 -0400

    add node selection of resources

--- em/src/deep_segmentation/extreme_points/run_experiments.py
+++ em/src/deep_segmentation/extreme_points/run_experiments.py
@@ -3,17 +3,18 @@ import numpy as np
 import os
 
 
-nodes_v = [ 'nukwa-04.cnca', 'nukwa-05.cnca']
-nodes_k = [ 'nukwa-00.cnca', 'nukwa-01.cnca'] #, 'nukwa-03.cnca']
+nodes_v = [ 'nukwa-04.cnca', 'nukwa-05.cnca', 'nukwa-06.cnca']
+nodes_k = [ 'nukwa-00.cnca', 'nukwa-01.cnca', 'nukwa-02.cnca' , 'nukwa-03.cnca']
 
 sizes = [ 72, 80, 88, 96, 104, 112, 120, 128 ]
 losses = ['CrossEntropy', 'Weigthed', 'Tversky', 'Dice']
+batch_sizes = [ 1, 2 ]
 optims = ['SGD', 'Adam']
-lrs = np.logspace(-4, -1, num=50)
-decays = np.logspace(-5, -2, num=50) 
-widths = [0, 2, 4, 6, 8 , 12]
-
-for j in range(10):
+lrs = np.logspace(-4, -2, num=5)
+decays = np.logspace(-6, -4, num=5) 
+widths = [0,4,8,12]
+alphas = [0.5, 0.6, 0.7, 0,8, 0.9]
+for j in range(3):
     base_content = """#!/bin/bash
 #SBATCH --job-name deep_protein 
 #SBATCH --time=7-00:00:00
@@ -39,14 +40,19 @@ export PYTHONPATH=$PYTHONPATH:/work/mzumbado/biostructure/em/src
         optim = np.random.choice(optims, size=1)[0]
         decay = np.random.choice(decays, size=1)[0]
         width = np.random.choice(widths, size=1)[0]
-        if size > 72:
+        batch_size = np.random.choice(batch_sizes, size=1)[0]
+        alpha = np.random.choice(alphas, size=1)[0]
+        beta = 1 - alpha
+        if (size > 72 | (size == 72 & batch_size == 2)):
             node = np.random.choice(nodes_v, size=1)[0]
             base_content = base_content.format('conda activate /work/mzumbado/EMAN2/envs/pytorch\n')
         else:
             node = np.random.choice(nodes_k, size=1)[0]
             base_content = base_content.format('conda activate /work/mzumbado/EMAN2/envs/pytorch_kepler\n')
-        cmd = "python run_.py run --with_amp=False --input_size={} --num_epochs=150 --optim={} --learning_rate={} --weight_decay={} --criterion={} --extra_width={} --validate_every=1" \
-               .format(size,optim,lr,decay,loss,width)
+        if loss in ['Weigthed', 'Tversky']:
+            cmd = "python run_.py run --with_amp=False --input_size={} --batch_size={} --alpha={} --beta={} --num_epochs=150 --optim={} --learning_rate={} --weight_decay={} --criterion={} --extra_width={} --validate_every=1".format(size,batch_size,alpha,beta,optim,lr,decay,loss,width)
+        else:
+            cmd = "python run_.py run --with_amp=False --input_size={} --batch_size={} --alpha={} --beta={} --num_epochs=100 --optim={} --learning_rate={} --weight_decay={} --criterion={} --extra_width={} --validate_every=1".format(size,batch_size,alpha,beta,optim,lr,decay,loss,width)
         print(base_content)
         print(cmd)
         n =jobfile.write(base_content)
